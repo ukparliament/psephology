cde1d6d6011ec3eee0e7514dfdcd64b54d098876
fantasticlife <michael@smethur.st:wq>
2023-09-29 21:40:36 +0100

    added routes for home, orders in council, boundary sets, constituency areas

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  get 'psephology' => 'home#index', as: :home
+  
+  get 'psephology/orders-in-council' => 'order_in_council#index', as: :order_in_council_list
+  get 'psephology/orders-in-council/:order_in_council' => 'order_in_council#show', as: :order_in_council_show
+  
+  get 'psephology/boundary-sets' => 'boundary_set#index', as: :boundary_set_list
+  get 'psephology/boundary-sets/:boundary_set' => 'boundary_set#show', as: :boundary_set_show
+  
+  get 'psephology/constituency-areas' => 'constituency_area#index', as: :constituency_area_list
+  get 'psephology/constituency-areas/current' => 'constituency_area#current', as: :constituency_area_list_current
+  get 'psephology/constituency-areas/all' => 'constituency_area#all', as: :constituency_area_list_all
+  get 'psephology/constituency-areas/:constituency_area' => 'constituency_area#show', as: :constituency_area_show
 end
