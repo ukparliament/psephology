c3165afb0cdbe81d75da7e8ab168bf9b2e268388
fantasticlife <michael@smethur.st:wq>
2023-09-29 21:41:32 +0100

    new methods for display title and constituency areas

--- app/models/boundary_set.rb
+++ app/models/boundary_set.rb
@@ -2,4 +2,22 @@ class BoundarySet < ApplicationRecord
   
   belongs_to :country
   belongs_to :order_in_council
+  
+  def display_title
+    display_title = self.country_name
+    display_title += ' (' + self.start_on.strftime( $DATE_DISPLAY_FORMAT ) + ' - ' 
+    display_title += self.end_on.strftime( $DATE_DISPLAY_FORMAT ) if self.end_on
+    display_title += ')'
+    display_title
+  end
+  
+  def constituency_areas
+    ConstituencyArea.find_by_sql(
+      "
+        SELECT *
+        FROM constituency_areas
+        WHERE boundary_set_id = #{self.id}
+      "
+    )
+  end
 end
