e1699c6f10a9892bc23065332b381279a2ab816f
fantasticlife <michael@smethur.st:wq>
2023-09-29 21:42:01 +0100

    added method for name with dates

--- app/models/constituency_area.rb
+++ app/models/constituency_area.rb
@@ -4,4 +4,12 @@ class ConstituencyArea < ApplicationRecord
   belongs_to :english_region, optional: true
   belongs_to :constituency_area_type
   belongs_to :boundary_set, optional: true
+  
+  def name_with_dates
+    name_with_dates = self.name
+    name_with_dates += ' (' + self.start_on.strftime( $DATE_DISPLAY_FORMAT ) + ' - '
+    name_with_dates += self.end_on.strftime( $DATE_DISPLAY_FORMAT ) if self.end_on
+    name_with_dates += ')'
+    name_with_dates
+  end
 end
