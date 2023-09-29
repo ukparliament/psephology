74e745a255f880b0c7e330e2268bdc4321289ef2
fantasticlife <michael@smethur.st:wq>
2023-09-29 15:36:38 +0100

    added association to boundary set

--- app/models/constituency_area.rb
+++ app/models/constituency_area.rb
@@ -3,4 +3,5 @@ class ConstituencyArea < ApplicationRecord
   belongs_to :country
   belongs_to :english_region, optional: true
   belongs_to :constituency_area_type
+  belongs_to :boundary_set, optional: true
 end
