20767997815004fb3f4ab919254aa07f834767b0
fantasticlife <michael@smethur.st:wq>
2023-09-29 21:42:30 +0100

    added mtjod to get boundary sets with country name

--- app/models/order_in_council.rb
+++ app/models/order_in_council.rb
@@ -1,3 +1,14 @@
 class OrderInCouncil < ApplicationRecord
   self.table_name = "orders_in_council"
+  
+  def boundary_sets
+    BoundarySet.find_by_sql(
+      "
+        SELECT bs.*, c.name AS country_name
+        FROM boundary_sets bs, countries c
+        WHERE bs.order_in_council_id = #{self.id}
+        AND bs.country_id = c.id
+      "
+    )
+  end
 end
