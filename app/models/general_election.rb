aa49330f07400f2e0333dd2742433fe67bc6408f
fantasticlife <michael@smethur.st:wq>
2023-09-30 10:01:01 +0100

    added methods to get elections in a country and in an english region

--- app/models/general_election.rb
+++ app/models/general_election.rb
@@ -1,2 +1,30 @@
 class GeneralElection < ApplicationRecord
+  
+  def elections_in_country( country )
+    Election.find_by_sql(
+      "
+        SELECT e.*, cg.name AS constituency_group_name
+        FROM elections e, constituency_groups cg, constituency_areas ca
+        WHERE e.general_election_id = #{self.id}
+        AND e.constituency_group_id = cg.id
+        AND cg.constituency_area_id = ca.id
+        AND ca.country_id = #{country.id}
+        ORDER BY cg.name
+      "
+    )
+  end
+  
+  def elections_in_english_region( english_region )
+    Election.find_by_sql(
+      "
+        SELECT e.*, cg.name AS constituency_group_name
+        FROM elections e, constituency_groups cg, constituency_areas ca
+        WHERE e.general_election_id = #{self.id}
+        AND e.constituency_group_id = cg.id
+        AND cg.constituency_area_id = ca.id
+        AND ca.english_region_id = #{english_region.id}
+        ORDER BY cg.name
+      "
+    )
+  end
 end
