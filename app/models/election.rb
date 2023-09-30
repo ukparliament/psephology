0689dea76c53a1f0fbf88f137c775541fbd73c9f
fantasticlife <michael@smethur.st:wq>
2023-09-30 12:01:26 +0100

    added method for by-election display title

--- app/models/election.rb
+++ app/models/election.rb
@@ -2,4 +2,10 @@ class Election < ApplicationRecord
   
   belongs_to :constituency_group
   belongs_to :general_election, optional: true
+  
+  def by_election_display_title
+    display_title = self.constituency_group_name
+    display_title += ' - ' + self.polling_on.strftime( $DATE_DISPLAY_FORMAT )
+    display_title
+  end
 end
