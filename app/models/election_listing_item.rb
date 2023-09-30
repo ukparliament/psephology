2d18ee747afdc37d65cb42f51f81076be92e617e
fantasticlife <michael@smethur.st:wq>
2023-09-30 14:54:21 +0100

    non-database model for storing a mix of general and by-elections

---
+++ app/models/election_listing_item.rb
@@ -0,0 +1,7 @@
+class ElectionListingItem
+  
+  attr_accessor :id
+  attr_accessor :type
+  attr_accessor :polling_on
+  attr_accessor :constituency_group_name
+end
---
+++ test/models/election_listing_item_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ElectionListingItemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
