eb8cc55aeedbc545a1cdf089c36a1d351a739c75
fantasticlife <michael@smethur.st:wq>
2023-09-30 14:57:15 +0100

    added candidacies method ordered by candidate names as per polling cards and results method ordered by vote

--- app/models/election.rb
+++ app/models/election.rb
@@ -8,4 +8,26 @@ class Election < ApplicationRecord
     display_title += ' - ' + self.polling_on.strftime( $DATE_DISPLAY_FORMAT )
     display_title
   end
+  
+  def candidacies
+    Candidacy.find_by_sql(
+      "
+        SELECT c.*
+        FROM candidacies c
+        WHERE c.election_id = #{self.id}
+        ORDER BY c.candidate_family_name, c.candidate_given_name
+      "
+    )
+  end
+  
+  def results
+    Candidacy.find_by_sql(
+      "
+        SELECT c.*
+        FROM candidacies c
+        WHERE c.election_id = #{self.id}
+        ORDER BY c.vote_count desc
+      "
+    )
+  end
 end
