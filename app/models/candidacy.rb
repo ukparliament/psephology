fd73afcfa3ad70011170453f33d380f83f30b51d
fantasticlife <michael@smethur.st:wq>
2023-09-30 14:55:58 +0100

    added candidate_polling_name method

--- app/models/candidacy.rb
+++ app/models/candidacy.rb
@@ -1,6 +1,12 @@
 class Candidacy < ApplicationRecord
   
   belongs_to :election
-  
   belongs_to :candidate_gender, class_name: 'Gender', foreign_key: :candidate_gender_id
+  
+  def candidate_polling_name
+    candidate_polling_name = self.candidate_family_name.upcase
+    candidate_polling_name += ', '
+    candidate_polling_name += self.candidate_given_name
+    candidate_polling_name
+  end
 end
