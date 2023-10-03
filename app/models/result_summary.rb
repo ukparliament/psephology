7ad91756d1fa69518da8b79153bb2f9c39ab5fb9
fantasticlife <michael@smethur.st:wq>
2023-10-03 10:27:26 +0100

    basic result summary model

---
+++ app/models/result_summary.rb
@@ -0,0 +1,2 @@
+class ResultSummary < ApplicationRecord
+end
---
+++ test/models/result_summary_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ResultSummaryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
