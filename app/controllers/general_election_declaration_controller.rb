90bf2dd6568e61b55e0bd1475e789a39a137abe1
fantasticlife <michael@smethur.st:wq>
2023-10-05 17:09:37 +0100

    basic controller and views for general election declarations

---
+++ app/controllers/general_election_declaration_controller.rb
@@ -0,0 +1,11 @@
+class GeneralElectionDeclarationController < ApplicationController
+  
+  def index
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find_by_polling_on( general_election )
+    raise ActiveRecord::RecordNotFound unless @general_election
+  
+    @elections = @general_election.elections_by_declaration
+    @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by declaration time"
+  end
+end
---
+++ test/controllers/general_election_declaration_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GeneralElectionDeclarationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
