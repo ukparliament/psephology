58c7e052588438a9c46fd273a4779c85de8ef9b6
fantasticlife <michael@smethur.st:wq>
2023-10-05 17:10:48 +0100

    formatting of time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   
   $DATE_DISPLAY_FORMAT = '%-d %B %Y'
   $DATE_TIME_DISPLAY_FORMAT = '%H:%M on %-d %B %Y'
+  $TIME_DISPLAY_FORMAT = '%H:%M'
 end
