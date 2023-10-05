d4253546743881eb317e7a0536fc8f208b7477ac
fantasticlife <michael@smethur.st:wq>
2023-10-05 15:41:01 +0100

    identify election by polling date and country by geography code

--- app/controllers/general_election_country_controller.rb
+++ app/controllers/general_election_country_controller.rb
@@ -2,16 +2,22 @@ class GeneralElectionCountryController < ApplicationController
   
   def index
     general_election = params[:general_election]
-    @general_election = GeneralElection.find( general_election )
+    @general_election = GeneralElection.find_by_polling_on( general_election )
+    raise ActiveRecord::RecordNotFound unless @general_election
+    
     @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - Countries"
     @countries = Country.all.order( 'name' )
   end
   
   def show
     general_election = params[:general_election]
-    @general_election = GeneralElection.find( general_election )
+    @general_election = GeneralElection.find_by_polling_on( general_election )
+    raise ActiveRecord::RecordNotFound unless @general_election
+    
     country = params[:country]
-    @country = Country.find( country )
+    @country = Country.find_by_geography_code( country )
+    raise ActiveRecord::RecordNotFound unless @country
+    
     @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name}"
     @english_regions = EnglishRegion.all.where( "country_id = ?", @country.id ).order( 'name' )
     @elections = @general_election.elections_in_country( @country)
