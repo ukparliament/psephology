7a99e4dffde8211f8367d94f6a28f75211c6f450
fantasticlife <michael@smethur.st:wq>
2023-10-05 11:00:30 +0100

    added debug

--- app/controllers/order_in_council_controller.rb
+++ app/controllers/order_in_council_controller.rb
@@ -7,7 +7,10 @@ class OrderInCouncilController < ApplicationController
   
   def show
     order_in_council = params[:order_in_council]
+    puts order_in_council
     @order_in_council = OrderInCouncil.find_by_url_key( order_in_council )
+    
+    puts @order_in_council.inspect
     raise ActiveRecord::RecordNotFound unless @order_in_council
     @boundary_sets = @order_in_council.boundary_sets
     @page_title = @order_in_council.title
