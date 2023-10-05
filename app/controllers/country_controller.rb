752420ef7c0e01a35a13e057f5dd148192fb89c3
fantasticlife <michael@smethur.st:wq>
2023-10-05 13:45:14 +0100

    basic controller and views for countries

---
+++ app/controllers/country_controller.rb
@@ -0,0 +1,14 @@
+class CountryController < ApplicationController
+  
+  def index
+    @page_title = 'Countries'
+    @countries = Country.all.order( 'name' )
+  end
+  
+  def show
+    country = params[:country]
+    @country = Country.find_by_geography_code( country )
+    raise ActiveRecord::RecordNotFound unless @country
+    @page_title = @country.name
+  end
+end
