396bf22bf90a056f3f06d0332b33bf7a4c4e8664
fantasticlife <michael@smethur.st:wq>
2023-10-06 21:38:35 +0100

    added methods for represented in election and won election

--- app/models/political_party.rb
+++ app/models/political_party.rb
@@ -1,2 +1,33 @@
 class PoliticalParty < ApplicationRecord
+  
+  def represented_in_election?( election )
+    represented_in_election = false
+    represented = Candidacy.find_by_sql( 
+      "
+        SELECT can.*
+        FROM candidacies can, certifications cert
+        WHERE can.election_id = #{election.id}
+        AND can.id = cert.candidacy_id
+        AND cert.political_party_id = #{self.id}
+      "
+    )
+    represented_in_election = true unless represented.empty?
+    represented_in_election
+  end
+  
+  def won_election?( election )
+    won_election = false
+    winning_candidates = Candidacy.find_by_sql( 
+      "
+        SELECT can.*
+        FROM candidacies can, certifications cert
+        WHERE can.election_id = #{election.id}
+        AND can.id = cert.candidacy_id
+        AND cert.political_party_id = #{self.id}
+        AND can.is_winning_candidacy IS TRUE
+      "
+    )
+    won_election = true unless winning_candidates.empty?
+    won_election
+  end
 end
