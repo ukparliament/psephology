149e569c08d91f0813685692ac039e3cd63e3c45
fantasticlife <michael@smethur.st:wq>
2023-10-10 21:14:03 +0100

    added order in council title to boundary set queries

--- app/controllers/country_boundary_set_general_election_controller.rb
+++ app/controllers/country_boundary_set_general_election_controller.rb
@@ -25,11 +25,12 @@ class CountryBoundarySetGeneralElectionController < ApplicationController
     boundary_set_date = Date.parse( boundary_set )
     @boundary_set = BoundarySet.find_by_sql(
       "
-        SELECT bs.*, c.name AS country_name
-        FROM boundary_sets bs, countries c
+        SELECT bs.*, c.name AS country_name, oic.title AS order_in_council_title
+        FROM boundary_sets bs, countries c, orders_in_council oic
         WHERE start_on = '#{boundary_set_date}'
         AND bs.country_id = c.id
         AND c.geography_code = '#{country}'
+        AND bs.order_in_council_id = oic.id
       "
     ).first
     raise ActiveRecord::RecordNotFound unless @boundary_set
@@ -66,11 +67,12 @@ class CountryBoundarySetGeneralElectionController < ApplicationController
     boundary_set_date = Date.parse( boundary_set )
     @boundary_set = BoundarySet.find_by_sql(
       "
-        SELECT bs.*, c.name AS country_name
-        FROM boundary_sets bs, countries c
+        SELECT bs.*, c.name AS country_name, oic.title AS order_in_council_title
+        FROM boundary_sets bs, countries c, orders_in_council oic
         WHERE start_on = '#{boundary_set_date}'
         AND bs.country_id = c.id
         AND c.geography_code = '#{country}'
+        AND bs.order_in_council_id = oic.id
       "
     ).first
     raise ActiveRecord::RecordNotFound unless @boundary_set
