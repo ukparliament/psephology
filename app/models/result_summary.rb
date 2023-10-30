a80016ac4792495e04383a0346e35a8a43a8c1be
fantasticlife <michael@smethur.st:wq>
2023-10-30 16:35:22 +0000

    added from political party and to political party methods

--- app/models/result_summary.rb
+++ app/models/result_summary.rb
@@ -1,2 +1,10 @@
 class ResultSummary < ApplicationRecord
+  
+  def from_political_party
+    PoliticalParty.find( self.from_political_party_id )
+  end
+  
+  def to_political_party
+    PoliticalParty.find( self.to_political_party_id )
+  end
 end
