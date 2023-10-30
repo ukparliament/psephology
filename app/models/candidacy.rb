70c7b01ccc4e604e7701c79e7042932ab7caec32
fantasticlife <michael@smethur.st:wq>
2023-10-30 16:32:22 +0000

    added main party listing abbreviation method

--- app/models/candidacy.rb
+++ app/models/candidacy.rb
@@ -9,4 +9,16 @@ class Candidacy < ApplicationRecord
     candidate_polling_name += self.candidate_given_name
     candidate_polling_name
   end
+  
+  def main_party_listing_abbreviation
+    main_party_listing_abbreviation = ''
+    if self.is_standing_as_commons_speaker == true
+      main_party_listing_abbreviation = 'Spk'
+    elsif self.is_standing_as_independent == true
+      main_party_listing_abbreviation = 'Ind'
+    else
+      main_party_listing_abbreviation = self.main_party_abbreviation
+    end
+    main_party_listing_abbreviation
+  end
 end
