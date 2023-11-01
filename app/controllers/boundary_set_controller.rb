6d7550cc1781b2162907e82aab6ca4e712743b30
fantasticlife <michael@smethur.st:wq>
2023-11-01 11:31:22 +0000

    geogrpahy code now geographic code

--- app/controllers/boundary_set_controller.rb
+++ app/controllers/boundary_set_controller.rb
@@ -3,7 +3,7 @@ class BoundarySetController < ApplicationController
   def index
     @boundary_sets = BoundarySet.find_by_sql(
       "
-        SELECT bs.*, c.name AS country_name, c.geography_code AS country_geography_code
+        SELECT bs.*, c.name AS country_name, c.geographic_code AS country_geographic_code
         FROM boundary_sets bs, countries c
         WHERE bs.country_id = c.id
         ORDER BY start_on, country_name
