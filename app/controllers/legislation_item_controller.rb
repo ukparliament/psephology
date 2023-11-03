757520fed38984e46906e784311428a855555072
fantasticlife <michael@smethur.st:wq>
2023-11-03 15:08:33 +0000

    basic legislation item controller

---
+++ app/controllers/legislation_item_controller.rb
@@ -0,0 +1,15 @@
+class LegislationItemController < ApplicationController
+  
+  def index
+    @page_title = 'Legislation'
+    @legislation_items = LegislationItem.all.order( 'statute_book_on desc' )
+  end
+  
+  def show
+    legislation_item = params[:legislation_item]
+    @legislation_item = LegislationItem.find_by_url_key( legislation_item )
+    @enabling_legislation = @legislation_item.enabling_legislation
+    @enabled_legislation = @legislation_item.enabled_legislation
+    @page_title = @legislation_item.title
+  end
+end
---
+++ test/controllers/legislation_item_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LegislationItemControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
