962178ae5603285565fa0892166552d9ef08d5ce
fantasticlife <michael@smethur.st:wq>
2023-11-03 15:23:14 +0000

    orders in council query now gets legislation items of that type, show action deleted

--- app/controllers/order_in_council_controller.rb
+++ app/controllers/order_in_council_controller.rb
@@ -1,15 +1,15 @@
 class OrderInCouncilController < ApplicationController
   
   def index
-    @orders_in_council = OrderInCouncil.all.order( 'made_on desc' )
+    @orders_in_council = LegislationItem.find_by_sql(
+      "
+        SELECT li.*
+        FROM legislation_items li, legislation_types lt
+        WHERE li.legislation_type_id = lt.id
+        AND lt.abbreviation = 'orders'
+        ORDER BY li.statute_book_on DESC
+      "
+    )
     @page_title = "Orders in Council"
-  end
-  
-  def show
-    order_in_council = params[:order_in_council]
-    @order_in_council = OrderInCouncil.find_by_url_key( order_in_council )
-    raise ActiveRecord::RecordNotFound unless @order_in_council
-    @boundary_sets = @order_in_council.boundary_sets
-    @page_title = @order_in_council.title
   end
 end
