766660e2ef24e8259e96ae88203892b6c7f6d74e
fantasticlife <michael@smethur.st:wq>
2023-11-04 21:59:35 +0000

    get the establishing legislation item title and general elections

--- app/controllers/country_boundary_party_switch_controller.rb
+++ app/controllers/country_boundary_party_switch_controller.rb
@@ -6,14 +6,18 @@ class CountryBoundaryPartySwitchController < ApplicationController
     
     @boundary_set = BoundarySet.find_by_sql(
       "
-        SELECT bs.*, c.name AS country_name
-        FROM boundary_sets bs, countries c
+        SELECT bs.*, c.name AS country_name, li.title as legislation_item_title
+        FROM boundary_sets bs, countries c, legislation_items li
         WHERE bs.country_id = c.id
         AND c.geographic_code = '#{country}'
         AND bs.start_on = '#{boundary_set}'
+        AND bs.legislation_item_id = li.id
       "
     ).first
     raise ActiveRecord::RecordNotFound unless @boundary_set
+    
+    # We get all the general elections held during the duration of the boundary set.
+    @general_elections = @boundary_set.general_elections
     
     @page_title = "Boundary set for #{@boundary_set.country_name} (#{@boundary_set.display_dates}) - party switches"
     
