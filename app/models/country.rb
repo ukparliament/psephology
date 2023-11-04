990b5ff15e16170146b862a6933b6cd45f33e76a
fantasticlife <michael@smethur.st:wq>
2023-11-04 22:22:10 +0000

    order boundary sets latest first

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,6 @@
 class Country < ApplicationRecord
   
   def boundary_sets
-    BoundarySet.all.where( "country_id = ?", self ).order( 'start_on' )
+    BoundarySet.all.where( "country_id = ?", self ).order( 'start_on DESC' )
   end
 end
