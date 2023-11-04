d23e11147317f2ab4823d2e234bf9b2e72df5f90
fantasticlife <michael@smethur.st:wq>
2023-11-04 22:24:25 +0000

    added boundary sets method

--- app/models/legislation_item.rb
+++ app/models/legislation_item.rb
@@ -25,4 +25,16 @@ class LegislationItem < ApplicationRecord
       "
     )
   end
+  
+  def boundary_sets
+    BoundarySet.find_by_sql(
+      "
+        SELECT bs.*, c.name AS country_name, c.geographic_code AS country_geographic_code
+        FROM boundary_sets bs, countries c
+        WHERE bs.legislation_item_id = #{self.id}
+        AND bs.country_id = c.id
+        ORDER BY bs.start_on DESC, country_name
+      "
+    )
+  end
 end
