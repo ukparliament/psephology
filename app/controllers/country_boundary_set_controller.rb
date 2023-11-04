e6dbf146a21c38e05f87650bf3b55e55698e9047
fantasticlife <michael@smethur.st:wq>
2023-11-04 21:59:56 +0000

    get the establishing legislation item title and general elections

--- app/controllers/country_boundary_set_controller.rb
+++ app/controllers/country_boundary_set_controller.rb
@@ -15,15 +15,19 @@ class CountryBoundarySetController < ApplicationController
     boundary_set_date = Date.parse( boundary_set )
     @boundary_set = BoundarySet.find_by_sql(
       "
-        SELECT bs.*, c.name AS country_name, oic.title AS order_in_council_title
-        FROM boundary_sets bs, countries c, orders_in_council oic
+        SELECT bs.*, c.name AS country_name, li.title AS legislation_item_title
+        FROM boundary_sets bs, countries c, legislation_items li
         WHERE start_on = '#{boundary_set_date}'
         AND bs.country_id = c.id
         AND c.geographic_code = '#{country}'
-        AND oic.id = bs.order_in_council_id
+        AND li.id = bs.legislation_item_id
       "
     ).first
     raise ActiveRecord::RecordNotFound unless @boundary_set
+    
+    # We get all the general elections held during the duration of the boundary set.
+    @general_elections = @boundary_set.general_elections
+    
     @constituency_areas = @boundary_set.constituency_areas
     @page_title = "Boundary set for #{@boundary_set.display_title} - constituency areas"
   end
