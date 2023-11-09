fc5e9345ab8c7cc926c66db65c3866a02eef892b
fantasticlife <michael@smethur.st:wq>
2023-11-09 17:14:44 +0000

    added association to member

--- app/models/candidacy.rb
+++ app/models/candidacy.rb
@@ -2,6 +2,7 @@ class Candidacy < ApplicationRecord
   
   belongs_to :election
   belongs_to :candidate_gender, class_name: 'Gender', foreign_key: :candidate_gender_id
+  belongs_to :member, optional: true
   
   def candidate_polling_name
     candidate_polling_name = self.candidate_family_name.upcase
