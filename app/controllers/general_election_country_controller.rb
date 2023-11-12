da180da0a9dfd0c2a6eccdf576f888fc37c39715
fantasticlife <michael@smethur.st:wq>
2023-11-12 21:47:53 +0000

    added multiline page title

--- app/controllers/general_election_country_controller.rb
+++ app/controllers/general_election_country_controller.rb
@@ -5,7 +5,9 @@ class GeneralElectionCountryController < ApplicationController
     @general_election = GeneralElection.find_by_polling_on( general_election )
     raise ActiveRecord::RecordNotFound unless @general_election
     
-    @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - Countries"
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - Countries"
+    
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>Countries</span>".html_safe
     @countries = Country.all.order( 'name' )
   end
   
@@ -18,7 +20,9 @@ class GeneralElectionCountryController < ApplicationController
     @country = Country.find_by_geographic_code( country )
     raise ActiveRecord::RecordNotFound unless @country
     
-    @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name}"
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name}"
+    
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name}</span>".html_safe
     @english_regions = EnglishRegion.all.where( "country_id = ?", @country.id ).order( 'name' )
     @elections = @general_election.elections_in_country( @country)
   end
