01d00602cf342b19d297e4f41fcb510485343ad9
fantasticlife <michael@smethur.st:wq>
2023-11-12 21:50:24 +0000

    added multiline page title

--- app/controllers/general_election_party_controller.rb
+++ app/controllers/general_election_party_controller.rb
@@ -7,6 +7,7 @@ class GeneralElectionPartyController < ApplicationController
     
     @party_performances = @general_election.party_performance
     
-    @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by party"
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by party"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By party</span>".html_safe
   end
 end
