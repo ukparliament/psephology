7512e4926b8513e3d249eabd7d7ed2dfca947d0f
fantasticlife <michael@smethur.st:wq>
2023-11-12 21:50:52 +0000

    added multiline page title

--- app/controllers/general_election_vote_share_controller.rb
+++ app/controllers/general_election_vote_share_controller.rb
@@ -6,6 +6,7 @@ class GeneralElectionVoteShareController < ApplicationController
     raise ActiveRecord::RecordNotFound unless @general_election
     
     @elections = @general_election.elections_by_vote_share
-    @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by vote share"
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by vote share"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By vote share</span>".html_safe
   end
 end
