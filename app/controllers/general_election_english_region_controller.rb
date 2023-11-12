fe3b3384899a1c8805293df7ba06f18d3760c369
fantasticlife <michael@smethur.st:wq>
2023-11-12 21:49:55 +0000

    added multiline page title

--- app/controllers/general_election_english_region_controller.rb
+++ app/controllers/general_election_english_region_controller.rb
@@ -9,7 +9,9 @@ class GeneralElectionEnglishRegionController < ApplicationController
     @country = Country.find_by_geographic_code( country )
     raise ActiveRecord::RecordNotFound unless @country
     
-    @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - English regions"
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - English regions"
+    
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>English regions</span>".html_safe
     @english_regions = EnglishRegion.all.where( "country_id = ?", @country.id ).order( 'name' )
   end
   
@@ -26,7 +28,11 @@ class GeneralElectionEnglishRegionController < ApplicationController
     @english_region = EnglishRegion.find_by_geographic_code( english_region )
     raise ActiveRecord::RecordNotFound unless @english_region
     
-    @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England"
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England"
+    
+
+    
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England</span>".html_safe
     @elections = @general_election.elections_in_english_region( @english_region)
   end
 end
