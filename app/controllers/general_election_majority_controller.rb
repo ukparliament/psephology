a8450928080281a85f44cd193835f6864a2a382a
fantasticlife <michael@smethur.st:wq>
2023-11-12 21:50:10 +0000

    added multiline page title

--- app/controllers/general_election_majority_controller.rb
+++ app/controllers/general_election_majority_controller.rb
@@ -6,6 +6,7 @@ class GeneralElectionMajorityController < ApplicationController
     raise ActiveRecord::RecordNotFound unless @general_election
   
     @elections = @general_election.elections_by_majority
-    @page_title = "UK general election - #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by majority"
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by majority"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By majority</span>".html_safe
   end
 end
