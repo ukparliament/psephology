edc937aa82b27ac31f30c90cf80c3053afdd3ebc
fantasticlife <michael@smethur.st:wq>
2023-11-12 14:52:34 +0000

    new methods for general election and by-elections

--- app/models/parliament_period.rb
+++ app/models/parliament_period.rb
@@ -6,4 +6,20 @@ class ParliamentPeriod < ApplicationRecord
     display_dates += self.dissolved_on.strftime( $DATE_DISPLAY_FORMAT ) if self.dissolved_on
     display_dates
   end
+  
+  def general_election
+    GeneralElection.all.where( "parliament_period_id = ?", self.id ).first
+  end
+  
+  def by_elections
+    Election.find_by_sql(
+      "
+        SELECT e.*, cg.name AS constituency_group_name
+        FROM elections e, constituency_groups cg
+        WHERE e.constituency_group_id = cg.id
+        AND e.general_election_id IS NULL
+        ORDER BY e.polling_on, constituency_group_name
+      "
+    )
+  end
 end
