35c57532cb7c47989d54ed5221e68907ab6638df
fantasticlife <michael@smethur.st:wq>
2023-11-12 14:17:57 +0000

    updated display dates method with new attribute names

--- app/models/parliament_period.rb
+++ app/models/parliament_period.rb
@@ -1,2 +1,9 @@
 class ParliamentPeriod < ApplicationRecord
+  
+  def display_dates
+    display_dates = self.summoned_on.strftime( $DATE_DISPLAY_FORMAT )
+    display_dates += ' - '
+    display_dates += self.dissolved_on.strftime( $DATE_DISPLAY_FORMAT ) if self.dissolved_on
+    display_dates
+  end
 end
