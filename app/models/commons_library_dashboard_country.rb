e556de15f15dc3eac359ad0e1891bc08046a2811
fantasticlife <michael@smethur.st:wq>
2023-11-13 15:03:31 +0000

    new model joining commons library dashboards to countries

---
+++ app/models/commons_library_dashboard_country.rb
@@ -0,0 +1,5 @@
+class CommonsLibraryDashboardCountry < ApplicationRecord
+  
+  belongs_to :commons_library_dashboard
+  belongs_to :country
+end
---
+++ test/models/commons_library_dashboard_country_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CommonsLibraryDashboardCountryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
