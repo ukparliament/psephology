2a7e7ba3a3e40492bfd072f42117750889f57be6
fantasticlife <michael@smethur.st:wq>
2023-11-16 10:54:46 +0000

    switched to primary key in url

--- app/controllers/general_election_english_region_controller.rb
+++ app/controllers/general_election_english_region_controller.rb
@@ -2,12 +2,10 @@ class GeneralElectionEnglishRegionController < ApplicationController
   
   def index
     general_election = params[:general_election]
-    @general_election = GeneralElection.find_by_polling_on( general_election )
-    raise ActiveRecord::RecordNotFound unless @general_election
+    @general_election = GeneralElection.find( general_election )
     
     country = params[:country]
-    @country = Country.find_by_geographic_code( country )
-    raise ActiveRecord::RecordNotFound unless @country
+    @country = Country.find( country )
     
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - English regions"
     
@@ -17,16 +15,13 @@ class GeneralElectionEnglishRegionController < ApplicationController
   
   def show
     general_election = params[:general_election]
-    @general_election = GeneralElection.find_by_polling_on( general_election )
-    raise ActiveRecord::RecordNotFound unless @general_election
+    @general_election = GeneralElection.find( general_election )
     
     country = params[:country]
-    @country = Country.find_by_geographic_code( country )
-    raise ActiveRecord::RecordNotFound unless @country
+    @country = Country.find( country )
     
     english_region = params[:english_region]
-    @english_region = EnglishRegion.find_by_geographic_code( english_region )
-    raise ActiveRecord::RecordNotFound unless @english_region
+    @english_region = EnglishRegion.find( english_region )
     
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England"
     
