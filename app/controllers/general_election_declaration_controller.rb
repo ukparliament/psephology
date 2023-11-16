f04998a8183a494356a6fc684d8d638e54fb90cd
fantasticlife <michael@smethur.st:wq>
2023-11-16 10:54:23 +0000

    switched to primary key in url

--- app/controllers/general_election_declaration_controller.rb
+++ app/controllers/general_election_declaration_controller.rb
@@ -2,8 +2,7 @@ class GeneralElectionDeclarationController < ApplicationController
   
   def index
     general_election = params[:general_election]
-    @general_election = GeneralElection.find_by_polling_on( general_election )
-    raise ActiveRecord::RecordNotFound unless @general_election
+    @general_election = GeneralElection.find( general_election )
   
     @elections = @general_election.elections_by_declaration
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by declaration time"
