41cb7d640859ab7d8e77ec97866444ededec9e34
fantasticlife <michael@smethur.st:wq>
2023-11-16 10:56:59 +0000

    switched to primary key in url

--- app/controllers/general_election_vote_share_controller.rb
+++ app/controllers/general_election_vote_share_controller.rb
@@ -2,8 +2,7 @@ class GeneralElectionVoteShareController < ApplicationController
   
   def index
     general_election = params[:general_election]
-    @general_election = GeneralElection.find_by_polling_on( general_election )
-    raise ActiveRecord::RecordNotFound unless @general_election
+    @general_election = GeneralElection.find( general_election )
     
     @elections = @general_election.elections_by_vote_share
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by vote share"
