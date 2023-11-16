f5b63b005e948f78157137efed4313ea59654b47
fantasticlife <michael@smethur.st:wq>
2023-11-16 10:56:24 +0000

    switched to primary key in url

--- app/controllers/general_election_party_controller.rb
+++ app/controllers/general_election_party_controller.rb
@@ -2,8 +2,7 @@ class GeneralElectionPartyController < ApplicationController
   
   def index
     general_election = params[:general_election]
-    @general_election = GeneralElection.find_by_polling_on( general_election )
-    raise ActiveRecord::RecordNotFound unless @general_election
+    @general_election = GeneralElection.find( general_election )
     
     @party_performances = @general_election.party_performance
     
