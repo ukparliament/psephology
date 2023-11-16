ba3e16d43157ce0502e94cfe0a7a472e3461f2e4
fantasticlife <michael@smethur.st:wq>
2023-11-16 10:57:29 +0000

    raises error if leg item not found

--- app/controllers/legislation_item_controller.rb
+++ app/controllers/legislation_item_controller.rb
@@ -8,6 +8,8 @@ class LegislationItemController < ApplicationController
   def show
     legislation_item = params[:legislation_item]
     @legislation_item = LegislationItem.find_by_url_key( legislation_item )
+    raise ActiveRecord::RecordNotFound unless @legislation_item
+    
     @enabling_legislation = @legislation_item.enabling_legislation
     @enabled_legislation = @legislation_item.enabled_legislation
     @boundary_sets = @legislation_item.boundary_sets
