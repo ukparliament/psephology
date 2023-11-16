a74ffd5b7c7f87611f9032fb7afb3ac360b54303
fantasticlife <michael@smethur.st:wq>
2023-11-16 10:57:50 +0000

    raises error if member not found

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -8,6 +8,7 @@ class MemberController < ApplicationController
   def show
     member = params[:member]
     @member = Member.find_by_mnis_id( member )
+    raise ActiveRecord::RecordNotFound unless @member
     @page_title = @member.display_name
     @elections_won = @member.elections_won
     @other_elections_contested = @member.other_elections_contested
