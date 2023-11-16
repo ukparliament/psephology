897d99db9942c84e32800992f507197e6f6068a2
fantasticlife <michael@smethur.st:wq>
2023-11-16 10:54:00 +0000

    switched to primary key in url

--- app/controllers/general_election_country_controller.rb
+++ app/controllers/general_election_country_controller.rb
@@ -2,23 +2,20 @@ class GeneralElectionCountryController < ApplicationController
   
   def index
     general_election = params[:general_election]
-    @general_election = GeneralElection.find_by_polling_on( general_election )
-    raise ActiveRecord::RecordNotFound unless @general_election
+    @general_election = GeneralElection.find( general_election )
     
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - Countries"
     
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>Countries</span>".html_safe
-    @countries = Country.all.order( 'name' )
+    @countries = @general_election.countries
   end
   
   def show
     general_election = params[:general_election]
-    @general_election = GeneralElection.find_by_polling_on( general_election )
-    raise ActiveRecord::RecordNotFound unless @general_election
+    @general_election = GeneralElection.find( general_election )
     
     country = params[:country]
-    @country = Country.find_by_geographic_code( country )
-    raise ActiveRecord::RecordNotFound unless @country
+    @country = Country.find( country )
     
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name}"
     
