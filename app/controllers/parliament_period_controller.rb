f3f6acc32718d0aa86bf9c6e8773b7a5cfcd3cd2
fantasticlife <michael@smethur.st:wq>
2023-11-16 10:58:14 +0000

    switched to primary key in url

--- app/controllers/parliament_period_controller.rb
+++ app/controllers/parliament_period_controller.rb
@@ -7,7 +7,8 @@ class ParliamentPeriodController < ApplicationController
   
   def show
     parliament_period = params[:parliament_period]
-    @parliament_period = ParliamentPeriod.find( parliament_period )
+    @parliament_period = ParliamentPeriod.find_by_number( parliament_period )
+    raise ActiveRecord::RecordNotFound unless @parliament_period
     @page_title = "Parliament #{@parliament_period.number}"
     @general_election = @parliament_period.general_election
     @by_elections = @parliament_period.by_elections
