e8c4aaf33f3e180ab20be8d1f4ad11fd556d21da
fantasticlife <michael@smethur.st:wq>
2023-11-20 21:19:40 +0000

    basic model

---
+++ app/models/boundary_set_legislation_item.rb
@@ -0,0 +1,5 @@
+class BoundarySetLegislationItem < ApplicationRecord
+  
+  belongs_to :boundary_set
+  belongs_to :legislation_item
+end
---
+++ test/models/boundary_set_legislation_item_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BoundarySetLegislationItemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
