bd933ddafdf26b68d902bf87209fcf99c3154a8f
fantasticlife <michael@smethur.st:wq>
2023-11-22 12:03:00 +0000

    removed elections by declaration time method

--- app/models/general_election.rb
+++ app/models/general_election.rb
@@ -167,64 +167,6 @@ class GeneralElection < ApplicationRecord
     )
   end
   
-  def elections_by_declaration
-    Election.find_by_sql(
-      "
-        SELECT e.*,
-          constituency_group.name AS constituency_group_name,
-          ( cast(e.majority as decimal ) / e.valid_vote_count ) AS majority_percentage,
-          winning_candidacy.candidate_given_name AS winning_candidacy_candidate_given_name,
-          winning_candidacy.candidate_family_name AS winning_candidacy_candidate_family_name,
-          winning_candidacy.is_standing_as_commons_speaker AS  winning_candidacy_standing_as_commons_speaker,
-          winning_candidacy.is_standing_as_independent AS  winning_candidacy_standing_as_independent,
-          winning_candidacy_party.party_id AS main_party_id,
-          winning_candidacy_party.party_name AS main_party_name,
-          winning_candidacy_party.party_abbreviation AS main_party_abbreviation,
-          winning_candidacy_adjunct_party.party_id AS adjunct_party_id,
-          winning_candidacy_adjunct_party.party_name AS adjunct_party_name,
-          winning_candidacy_adjunct_party.party_abbreviation AS adjunct_party_abbreviation
-        FROM elections e
-        
-        INNER JOIN (
-          SELECT *
-          FROM constituency_groups
-        ) constituency_group
-        ON constituency_group.id = e.constituency_group_id
-        
-        INNER JOIN (
-          SELECT c.*
-          FROM candidacies c
-          WHERE c.is_winning_candidacy IS TRUE
-        ) winning_candidacy
-        ON winning_candidacy.election_id = e.id
-        
-        LEFT JOIN (
-          SELECT c.*, pp.id AS party_id, pp.name AS party_name, pp.abbreviation AS party_abbreviation
-          FROM candidacies c, certifications cert, political_parties pp
-          WHERE cert.candidacy_id = c.id
-          AND cert.political_party_id = pp.id
-          AND cert.adjunct_to_certification_id IS NULL
-          AND c.is_winning_candidacy IS TRUE
-        ) winning_candidacy_party
-        ON winning_candidacy_party.election_id = e.id
-        
-        LEFT JOIN (
-          SELECT c.*, pp.id AS party_id, pp.name AS party_name, pp.abbreviation AS party_abbreviation
-          FROM candidacies c, certifications cert, political_parties pp
-          WHERE cert.candidacy_id = c.id
-          AND cert.political_party_id = pp.id
-          AND cert.adjunct_to_certification_id IS NOT NULL
-          AND c.is_winning_candidacy IS TRUE
-        ) winning_candidacy_adjunct_party
-        ON winning_candidacy_adjunct_party.election_id = e.id
-      
-        
-        WHERE general_election_id = #{self.id}
-        ORDER BY declaration_at
-      "
-    )
-  end
-  
   def elections_by_vote_share
     Election.find_by_sql(
       "
