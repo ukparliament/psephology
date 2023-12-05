d27422c552af7210956be75aa24c4910d9ba24db
fantasticlife <michael@smethur.st:wq>
2023-12-05 09:20:26 +0000

    new methods for elections contested and won

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,12 +8,64 @@ class Member < ApplicationRecord
     self.family_name + ', ' + self.given_name
   end
   
+  def elections
+    Election.find_by_sql(
+      "
+        SELECT
+          e.*,
+          constituency_group.name AS constituency_name,
+          constituency_area.id AS constituency_area_id,
+          candidacy.result_position AS candidacy_result_position,
+          candidacy.is_winning_candidacy AS candidacy_is_winning_candidacy,
+          candidacy.vote_count AS candidacy_vote_count,
+          candidacy.vote_share AS candidacy_vote_share,
+          general_election.polling_on AS general_election_polling_on
+        
+        FROM elections e
+        
+        INNER JOIN (
+          SELECT *
+          FROM candidacies
+          WHERE member_id = #{self.id}
+        ) candidacy
+        ON candidacy.election_id = e.id
+        
+        INNER JOIN (
+          SELECT * 
+          FROM constituency_groups
+        ) constituency_group
+        ON constituency_group.id = e.constituency_group_id
+        
+        LEFT JOIN (
+          SELECT ca.*, cg.id AS constituency_group_id
+          FROM constituency_groups cg, constituency_areas ca
+          WHERE cg.constituency_area_id = ca.id
+        ) constituency_area
+        ON constituency_area.constituency_group_id = e.constituency_group_id
+        
+        LEFT JOIN (
+          SELECT *
+          FROM general_elections
+        ) general_election
+        ON general_election.id = e.general_election_id
+        
+        ORDER BY e.polling_on DESC
+      "
+    )
+  end
+  
   def elections_won
     Election.find_by_sql(
       "
         SELECT
           e.*,
-          constituency.name AS constituency_name
+          constituency_group.name AS constituency_name,
+          constituency_area.id AS constituency_area_id,
+          candidacy.result_position AS candidacy_result_position,
+          candidacy.is_winning_candidacy AS candidacy_is_winning_candidacy,
+          candidacy.vote_count AS candidacy_vote_count,
+          candidacy.vote_share AS candidacy_vote_share,
+          general_election.polling_on AS general_election_polling_on
         
         FROM elections e
         
@@ -22,42 +74,27 @@ class Member < ApplicationRecord
           FROM candidacies
           WHERE member_id = #{self.id}
           AND is_winning_candidacy IS TRUE
-        ) winning_candidacy
-        ON winning_candidacy.election_id = e.id
+        ) candidacy
+        ON candidacy.election_id = e.id
         
         INNER JOIN (
           SELECT * 
           FROM constituency_groups
-        ) constituency
-        ON constituency.id = e.constituency_group_id
+        ) constituency_group
+        ON constituency_group.id = e.constituency_group_id
         
-        ORDER BY e.polling_on DESC
-      "
-    )
-  end
-  
-  def other_elections_contested
-    Election.find_by_sql(
-      "
-        SELECT
-          e.*,
-          constituency.name AS constituency_name
+        LEFT JOIN (
+          SELECT ca.*, cg.id AS constituency_group_id
+          FROM constituency_groups cg, constituency_areas ca
+          WHERE cg.constituency_area_id = ca.id
+        ) constituency_area
+        ON constituency_area.constituency_group_id = e.constituency_group_id
         
-        FROM elections e
-        
-        INNER JOIN (
+        LEFT JOIN (
           SELECT *
-          FROM candidacies
-          WHERE member_id = #{self.id}
-          AND is_winning_candidacy IS FALSE
-        ) winning_candidacy
-        ON winning_candidacy.election_id = e.id
-        
-        INNER JOIN (
-          SELECT * 
-          FROM constituency_groups
-        ) constituency
-        ON constituency.id = e.constituency_group_id
+          FROM general_elections
+        ) general_election
+        ON general_election.id = e.general_election_id
         
         ORDER BY e.polling_on DESC
       "
