164f512f75d5d7f843499af13baa079dbea22585
fantasticlife <michael@smethur.st:wq>
2023-12-06 02:32:07 +0000

    new very basic show view

--- app/controllers/general_election_party_controller.rb
+++ app/controllers/general_election_party_controller.rb
@@ -9,4 +9,14 @@ class GeneralElectionPartyController < ApplicationController
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by party"
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By party</span>".html_safe
   end
+  
+  def show
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find( general_election )
+    
+    political_party = params[:political_party]
+    @political_party = PoliticalParty.find( political_party )
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@political_party.name}"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@political_party.name}</span>".html_safe
+  end
 end
