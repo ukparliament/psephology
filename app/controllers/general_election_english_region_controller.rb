6c8163d14b803c2b074a7f734738acc536ac325a
fantasticlife <michael@smethur.st:wq>
2023-12-07 17:06:54 +0000

    uses english regions in country method so won't break for older general elections in constituency areas that don't fit english regions

--- app/controllers/general_election_english_region_controller.rb
+++ app/controllers/general_election_english_region_controller.rb
@@ -10,7 +10,8 @@ class GeneralElectionEnglishRegionController < ApplicationController
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - English regions"
     
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>English regions</span>".html_safe
-    @english_regions = EnglishRegion.all.where( "country_id = ?", @country.id ).order( 'name' )
+    @english_regions = @general_election.english_regions_in_country( @country )
+    raise ActiveRecord::RecordNotFound if @english_regions.empty?
   end
   
   def show
@@ -24,8 +25,6 @@ class GeneralElectionEnglishRegionController < ApplicationController
     @english_region = EnglishRegion.find( english_region )
     
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England"
-    
-
     
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England</span>".html_safe
     @elections = @general_election.elections_in_english_region( @english_region)
