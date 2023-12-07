c7b88f0bea52ca74883ede8c3d27179727e4a81b
fantasticlife <michael@smethur.st:wq>
2023-12-07 17:46:58 +0000

    added current constituencies method

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,4 +3,17 @@ class Country < ApplicationRecord
   def boundary_sets
     BoundarySet.all.where( "country_id = ?", self ).order( 'start_on DESC' )
   end
+  
+  def current_constituencies
+    ConstituencyArea.find_by_sql(
+      "
+        SELECT ca.*
+        FROM constituency_areas ca, boundary_sets bs
+        WHERE ca.boundary_set_id = bs.id
+        AND ca.country_id = #{self.id}
+        AND bs.end_on IS NULL
+        ORDER BY ca.name
+      "
+    )
+  end
 end
