6a9fb3b1c6bd6b9dc9c4a562f3f56450aaad8ef3
fantasticlife <michael@smethur.st:wq>
2023-12-14 15:37:23 +0000

    new vote share method

--- app/models/general_election_party_performance.rb
+++ app/models/general_election_party_performance.rb
@@ -2,4 +2,8 @@ class GeneralElectionPartyPerformance < ApplicationRecord
   
   belongs_to :general_election
   belongs_to :political_party
+  
+  def vote_share
+    ( ( self.cumulative_vote_count.to_d / self.general_election_valid_vote_count ) * 100 ).truncate( 1 )
+  end
 end
