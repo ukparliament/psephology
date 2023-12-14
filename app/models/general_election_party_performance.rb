6dea54662e01d1764baf69f5bf635c21ccbffc98
fantasticlife <michael@smethur.st:wq>
2023-12-14 17:31:47 +0000

    round not truncate

--- app/models/general_election_party_performance.rb
+++ app/models/general_election_party_performance.rb
@@ -4,6 +4,6 @@ class GeneralElectionPartyPerformance < ApplicationRecord
   belongs_to :political_party
   
   def vote_share
-    ( ( self.cumulative_vote_count.to_d / self.general_election_valid_vote_count ) * 100 ).truncate( 1 )
+    ( ( self.cumulative_vote_count.to_f / self.general_election_valid_vote_count.to_f ) * 100 ).round( 1 )
   end
 end
