35555679f46cd8b0e19207f9a3152c1bbfc157cd
fantasticlife <michael@smethur.st:wq>
2023-12-19 08:32:57 +0000

    show method now includes associated parliament period number

--- app/controllers/general_election_controller.rb
+++ app/controllers/general_election_controller.rb
@@ -7,7 +7,16 @@ class GeneralElectionController < ApplicationController
   
   def show
     general_election = params[:general_election]
-    @general_election = GeneralElection.find( general_election )
+    @general_election = GeneralElection.find_by_sql(
+      "
+        SELECT ge.*, pp.number AS parliament_period_number
+        FROM general_elections ge, parliament_periods pp
+        WHERE ge.parliament_period_id = pp.id
+        AND ge.id = #{general_election}
+      "
+    ).first
+    raise ActiveRecord::RecordNotFound unless @general_election
+    
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by area"
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By area</span>".html_safe
     @countries = @general_election.countries
