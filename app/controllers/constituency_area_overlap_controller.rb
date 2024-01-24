3b411f9e5ac62c5981de4866bb3048feeb88da79
fantasticlife <michael@smethur.st:wq>
2024-01-24 10:48:36 +0000

    basic controller and views

---
+++ app/controllers/constituency_area_overlap_controller.rb
@@ -0,0 +1,73 @@
+class ConstituencyAreaOverlapController < ApplicationController
+  
+  def index
+    @page_title = 'Constituency area overlaps'
+    order = params[:order]
+    if order == 'to-constituency'
+      @constituency_area_overlaps = ConstituencyAreaOverlap.find_by_sql(
+        "
+          SELECT cao.*, from_ca.name AS from_constituency_name, to_ca.name AS to_constituency_name
+          FROM constituency_area_overlaps cao, constituency_areas from_ca, constituency_areas to_ca
+          WHERE cao.from_constituency_area_id = from_ca.id
+          AND cao.to_constituency_area_id = to_ca.id
+          ORDER BY to_constituency_name
+        "
+      )
+    else
+      @constituency_area_overlaps = ConstituencyAreaOverlap.find_by_sql(
+        "
+          SELECT cao.*, from_ca.name AS from_constituency_name, to_ca.name AS to_constituency_name
+          FROM constituency_area_overlaps cao, constituency_areas from_ca, constituency_areas to_ca
+          WHERE cao.from_constituency_area_id = from_ca.id
+          AND cao.to_constituency_area_id = to_ca.id
+          ORDER BY from_constituency_name
+        "
+      )
+    end
+  end
+  
+  def show
+    constituency_area = params[:constituency_area]
+
+    @constituency_area = ConstituencyArea.find_by_sql(
+      "
+        SELECT ca.*, boundary_set.start_on AS start_on, boundary_set.end_on AS end_on, constituency_area_type.area_type, country.country_name, country.geographic_code AS country_geographic_code, english_region.english_region_name
+        
+        FROM constituency_areas ca
+        
+        LEFT JOIN (
+          SELECT id AS english_region_id, name AS english_region_name
+          FROM english_regions
+        ) AS english_region
+        ON english_region.english_region_id = ca.english_region_id
+        
+        INNER JOIN (
+          SELECT id AS boundary_set_id, start_on, end_on
+          FROM boundary_sets
+        ) boundary_set
+        ON boundary_set.boundary_set_id = ca.boundary_set_id
+        
+        INNER JOIN (
+          SELECT id AS constituency_area_type_id, area_type
+          FROM constituency_area_types
+        ) constituency_area_type
+        ON constituency_area_type.constituency_area_type_id = ca.constituency_area_type_id
+        
+        INNER JOIN (
+          SELECT id AS country_id, name AS country_name, geographic_code AS geographic_code
+          FROM countries
+        ) country
+        ON country.country_id = ca.country_id
+        
+        WHERE ca.id = '#{constituency_area}'
+      "
+    ).first
+    raise ActiveRecord::RecordNotFound unless @constituency_area
+    
+    @overlaps_from = @constituency_area.overlaps_from
+    @overlaps_to = @constituency_area.overlaps_to
+    
+    @page_title = @constituency_area.name_with_dates
+    @multiline_page_title = "#{@constituency_area.name_with_dates} <span class='subhead'>Overlaps</span>".html_safe
+  end
+end
---
+++ test/controllers/constituency_area_overlap_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ConstituencyAreaOverlapControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
