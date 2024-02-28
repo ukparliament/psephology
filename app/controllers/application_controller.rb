259239e2c2d46c0ede583123b084bc7b2cc6eb79
fantasticlife <michael@smethur.st:wq>
2024-02-28 14:36:23 +0000

    added DECLARATION_TIME_DISPLAY_FORMAT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   
   $DATE_DISPLAY_FORMAT = '%-d %B %Y'
   $DATE_TIME_DISPLAY_FORMAT = '%H:%M on %-d %B %Y'
+  $DECLARATION_TIME_DISPLAY_FORMAT = '%A %-d at %H:%M'
   $TIME_DISPLAY_FORMAT = '%H:%M'
 end
