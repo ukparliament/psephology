2840fa391c1b15187d4ce4a9c03e1757fe18cbef
fantasticlife <michael@smethur.st:wq>
2024-02-29 13:20:22 +0000

    added url mapping method

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -7,4 +7,17 @@ class MetaController < ApplicationController
   def schema
     @page_title = 'Schema'
   end
+  
+  def url_mapping
+    @page_title = 'URL mapping'
+    
+    @elections = Election.find_by_sql(
+      "
+        SELECT e.*, cg.name AS constituency_group_name
+        FROM elections e, constituency_groups cg
+        WHERE e.is_notional IS FALSE
+        AND e.constituency_group_id = cg.id
+      "
+    )
+  end
 end
