75b303f4e71e1e34b0efff312fc38d041822f8bc
fantasticlife <michael@smethur.st:wq>
2024-03-01 15:32:45 +0000

    index only shows top level countries with elections

--- app/controllers/general_election_country_controller.rb
+++ app/controllers/general_election_country_controller.rb
@@ -7,7 +7,7 @@ class GeneralElectionCountryController < ApplicationController
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - Countries"
     
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>Countries</span>".html_safe
-    @countries = @general_election.countries
+    @countries = @general_election.top_level_countries_with_elections
   end
   
   def show
@@ -20,6 +20,7 @@ class GeneralElectionCountryController < ApplicationController
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name}"
     
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name}</span>".html_safe
+    @countries = @general_election.child_countries_with_elections_in_country( @country )
     @english_regions = @general_election.english_regions_in_country( @country )
     @elections = @general_election.elections_in_country( @country)
   end
