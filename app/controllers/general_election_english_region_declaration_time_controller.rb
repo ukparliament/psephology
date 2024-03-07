eb96f96886a4c862d59b714fafff231d0fdf88ec
fantasticlife <michael@smethur.st:wq>
2024-03-07 11:58:22 +0000

    basic controller and views

---
+++ app/controllers/general_election_english_region_declaration_time_controller.rb
@@ -0,0 +1,25 @@
+class GeneralElectionEnglishRegionDeclarationTimeController < ApplicationController
+  
+  def index
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find_by_sql(
+      "
+        SELECT ge.*, pp.number AS parliament_period_number
+        FROM general_elections ge, parliament_periods pp
+        WHERE ge.parliament_period_id = pp.id
+        AND ge.id = #{general_election}
+      "
+    ).first
+    
+    country = params[:country]
+    english_region = params[:english_region]
+    @english_region = EnglishRegion.all.where( 'id = ?', english_region ).where( 'country_id =?', country).first
+    
+    raise ActiveRecord::RecordNotFound unless @general_election and @english_region
+  
+    @elections = @general_election.elections_by_declaration_time_in_english_region( @english_region )
+    
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by declaration time"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by declaration time</span>".html_safe
+  end
+end
---
+++ app/helpers/general_election_english_region_declaration_time_helper.rb
@@ -0,0 +1,2 @@
+module GeneralElectionEnglishRegionDeclarationTimeHelper
+end
