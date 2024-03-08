d2fef144c75e41fc93e5094475517e72e9d1746b
fantasticlife <michael@smethur.st:wq>
2024-03-08 20:03:20 +0000

    added vote share method

--- app/models/country_general_election_party_performance.rb
+++ app/models/country_general_election_party_performance.rb
@@ -3,4 +3,8 @@ class CountryGeneralElectionPartyPerformance < ApplicationRecord
   belongs_to :general_election
   belongs_to :political_party
   belongs_to :country
+  
+  def vote_share( valid_vote_count )
+    ( ( self.cumulative_vote_count.to_f / valid_vote_count.to_f ) * 100 ).round( 1 )
+  end
 end
