0504410304bb99a46a0ba837f7f27e2edaa6c496
fantasticlife <michael@smethur.st:wq>
2024-03-08 20:02:37 +0000

    basic controller and view

---
+++ app/controllers/general_election_country_political_party_controller.rb
@@ -0,0 +1,25 @@
+class GeneralElectionCountryPoliticalPartyController < ApplicationController
+  
+  def index
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find_by_sql(
+      "
+        SELECT ge.*, pp.number AS parliament_period_number
+        FROM general_elections ge, parliament_periods pp
+        WHERE ge.parliament_period_id = pp.id
+        AND ge.id = #{general_election}
+      "
+    ).first
+    raise ActiveRecord::RecordNotFound unless @general_election
+    
+    country = params[:country]
+    @country = Country.find( country )
+    
+    @party_performances = @general_election.party_performance_in_country( @country )
+    
+    @valid_vote_count_in_general_election_in_country = @general_election.valid_vote_count_in_country( @country )
+    
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - by party"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - by party</span>".html_safe
+  end
+end
---
+++ app/helpers/general_election_country_political_party_helper.rb
@@ -0,0 +1,2 @@
+module GeneralElectionCountryPoliticalPartyHelper
+end
