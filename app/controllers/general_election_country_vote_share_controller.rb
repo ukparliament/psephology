e6de8d10c18e24c06c963621190e9e20da118331
fantasticlife <michael@smethur.st:wq>
2024-03-08 17:47:28 +0000

    basic controller and view

---
+++ app/controllers/general_election_country_vote_share_controller.rb
@@ -0,0 +1,23 @@
+class GeneralElectionCountryVoteShareController < ApplicationController
+  
+  def index
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find_by_sql(
+      "
+        SELECT ge.*, pp.number AS parliament_period_number
+        FROM general_elections ge, parliament_periods pp
+        WHERE ge.parliament_period_id = pp.id
+        AND ge.id = #{general_election}
+      "
+    ).first
+    raise ActiveRecord::RecordNotFound unless @general_election
+    
+    country = params[:country]
+    @country = Country.find( country )
+    
+    @elections = @general_election.elections_by_vote_share_in_country( @country )
+    
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - by vote share"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - by vote share</span>".html_safe
+  end
+end
