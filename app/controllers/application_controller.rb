f8c063dcd39009c4ace32284866f98d330ae770e
fantasticlife <michael@smethur.st:wq>
2024-03-09 19:22:15 +0000

    added coverage period to describe general elections covered

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,5 @@ class ApplicationController < ActionController::Base
   $DATE_TIME_DISPLAY_FORMAT = '%H:%M on %-d %B %Y'
   $DECLARATION_TIME_DISPLAY_FORMAT = '%A %-d at %H:%M'
   $TIME_DISPLAY_FORMAT = '%H:%M'
+  $COVERAGE_PERIOD = '2015 - 2019'
 end
