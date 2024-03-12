57333ab6a07d0d9481594c81dd5e7efd1b639644
fantasticlife <michael@smethur.st:wq>
2024-03-12 13:21:57 +0000

    basic controller and views

---
+++ app/controllers/general_election_english_region_party_election_controller.rb
@@ -0,0 +1,56 @@
+class GeneralElectionEnglishRegionPartyElectionController < ApplicationController
+  
+  def index
+
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find_by_sql(
+      "
+        SELECT ge.*, pp.number AS parliament_period_number
+        FROM general_elections ge, parliament_periods pp
+        WHERE ge.parliament_period_id = pp.id
+        AND ge.id = #{general_election}
+      "
+    ).first
+    
+    country = params[:country]
+    english_region = params[:english_region]
+    @english_region = EnglishRegion.all.where( 'id = ?', english_region ).where( 'country_id =?', country).first
+    
+    raise ActiveRecord::RecordNotFound unless @general_election and @english_region
+    
+    political_party = params[:political_party]
+    @political_party = PoliticalParty.find( political_party )
+    
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - Elections contested by #{@political_party.name}"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - Elections contested by #{@political_party.name}</span>".html_safe
+    
+    @elections_contested = @political_party.elections_contested_in_general_election_in_english_region( @general_election, @english_region )
+  end
+  
+  def won
+
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find_by_sql(
+      "
+        SELECT ge.*, pp.number AS parliament_period_number
+        FROM general_elections ge, parliament_periods pp
+        WHERE ge.parliament_period_id = pp.id
+        AND ge.id = #{general_election}
+      "
+    ).first
+    
+    country = params[:country]
+    english_region = params[:english_region]
+    @english_region = EnglishRegion.all.where( 'id = ?', english_region ).where( 'country_id =?', country).first
+    
+    raise ActiveRecord::RecordNotFound unless @general_election and @english_region
+    
+    political_party = params[:political_party]
+    @political_party = PoliticalParty.find( political_party )
+    
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - Elections won by #{@political_party.name}"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - Elections won by #{@political_party.name}</span>".html_safe
+    
+    @elections_won = @political_party.elections_won_in_general_election_in_english_region( @general_election, @english_region )
+  end
+end
---
+++ app/helpers/general_election_english_region_party_election_helper.rb
@@ -0,0 +1,2 @@
+module GeneralElectionEnglishRegionPartyElectionHelper
+end
