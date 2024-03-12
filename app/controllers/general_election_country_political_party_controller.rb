20675edb1bb8e51d0e3a132f266f0bc212ca1237
fantasticlife <michael@smethur.st:wq>
2024-03-12 18:07:06 +0000

    added show method

--- app/controllers/general_election_country_political_party_controller.rb
+++ app/controllers/general_election_country_political_party_controller.rb
@@ -22,4 +22,28 @@ class GeneralElectionCountryPoliticalPartyController < ApplicationController
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - by party"
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - by party</span>".html_safe
   end
+  
+  def show
+
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find_by_sql(
+      "
+        SELECT ge.*, pp.number AS parliament_period_number
+        FROM general_elections ge, parliament_periods pp
+        WHERE ge.parliament_period_id = pp.id
+        AND ge.id = #{general_election}
+      "
+    ).first
+    
+    raise ActiveRecord::RecordNotFound unless @general_election
+    
+    country = params[:country]
+    @country = Country.find( country )
+    
+    political_party = params[:political_party]
+    @political_party = PoliticalParty.find( political_party )
+    
+    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - #{@political_party.name}"
+    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - #{@political_party.name}</span>".html_safe
+  end
 end
