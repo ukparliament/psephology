3a63eef44f86c9297d9a52db23240006772fa638
fantasticlife <michael@smethur.st:wq>
2024-03-12 13:22:52 +0000

    new methods for elections contested and won in general election in english region

--- app/models/political_party.rb
+++ app/models/political_party.rb
@@ -162,6 +162,112 @@ class PoliticalParty < ApplicationRecord
     )
   end
   
+  def elections_contested_in_general_election_in_english_region( general_election, english_region )
+    Election.find_by_sql(
+      "
+        SELECT e.*,
+          constituency_group.name AS constituency_name,
+          candidacy.candidate_given_name AS winning_candidacy_candidate_given_name,
+          candidacy.candidate_family_name AS winning_candidacy_candidate_family_name,
+          candidacy.vote_count AS candidacy_vote_count,
+          candidacy.vote_share AS candidacy_vote_share,
+          candidacy.vote_change AS candidacy_vote_change,
+          candidacy.result_position AS candidacy_result_position,
+          member.mnis_id AS member_mnis_id,
+          electorate.population_count AS electorate_population_count
+        FROM elections e
+      
+        INNER JOIN (
+          SELECT can.*
+          FROM candidacies can, certifications cert
+  	      WHERE can.id = cert.candidacy_id
+          AND cert.political_party_id = #{self.id}
+  	      AND cert.adjunct_to_certification_id IS NULL
+        ) candidacy
+        ON candidacy.election_id = e.id
+        
+        INNER JOIN (
+          SELECT cg.*
+          FROM constituency_groups cg, constituency_areas ca
+          WHERE cg.constituency_area_id = ca.id
+          AND ca.english_region_id = #{english_region.id}
+        ) constituency_group
+        ON constituency_group.id = e.constituency_group_id
+      
+        LEFT JOIN (
+          SELECT m.*, can.election_id AS election_id
+          FROM members m, candidacies can, certifications cert
+          WHERE m.id = can.member_id
+  	      AND can.id = cert.candidacy_id
+          AND cert.political_party_id = #{self.id}
+  	      AND cert.adjunct_to_certification_id IS NULL
+        ) member
+        ON member.election_id = e.id
+      
+        LEFT JOIN (
+          SELECT *
+          FROM electorates
+        ) electorate
+        ON electorate.id = e.electorate_id
+      
+        WHERE e.general_election_id = #{general_election.id}
+        ORDER BY constituency_name
+      "
+    )
+  end
+  
+  def elections_won_in_general_election_in_english_region( general_election, english_region )
+    Election.find_by_sql(
+      "
+        SELECT e.*,
+          constituency_group.name AS constituency_name,
+          winning_candidacy.candidate_given_name AS winning_candidacy_candidate_given_name,
+          winning_candidacy.candidate_family_name AS winning_candidacy_candidate_family_name,
+          winning_candidacy.vote_count AS winning_candidacy_vote_count,
+          winning_candidacy.vote_share AS winning_candidacy_vote_share,
+          winning_candidacy.vote_change AS winning_candidacy_vote_change,
+          member.mnis_id AS member_mnis_id,
+          electorate.population_count AS electorate_population_count
+        FROM elections e
+      
+        INNER JOIN (
+          SELECT can.*
+          FROM candidacies can, certifications cert
+  	      WHERE can.is_winning_candidacy IS TRUE
+  	      AND can.id = cert.candidacy_id
+          AND cert.political_party_id = #{self.id}
+  	      AND cert.adjunct_to_certification_id IS NULL
+        ) winning_candidacy
+        ON winning_candidacy.election_id = e.id
+      
+        LEFT JOIN (
+          SELECT m.*, can.election_id AS election_id
+          FROM members m, candidacies can
+          WHERE m.id = can.member_id
+  	      AND can.is_winning_candidacy IS TRUE
+        ) member
+        ON member.election_id = e.id
+        
+        INNER JOIN (
+          SELECT cg.*
+          FROM constituency_groups cg, constituency_areas ca
+          WHERE cg.constituency_area_id = ca.id
+          AND ca.english_region_id = #{english_region.id}
+        ) constituency_group
+        ON constituency_group.id = e.constituency_group_id
+      
+        LEFT JOIN (
+          SELECT *
+          FROM electorates
+        ) electorate
+        ON electorate.id = e.electorate_id
+      
+        WHERE e.general_election_id = #{general_election.id}
+        ORDER BY constituency_name
+      "
+    )
+  end
+  
   def general_elections
     GeneralElectionPartyPerformance.find_by_sql(
       "
