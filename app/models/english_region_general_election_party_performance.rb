1891cf296b7f4c177b5412bd3bedc586fa3e6193
fantasticlife <michael@smethur.st:wq>
2024-03-18 13:13:24 +0000

    added party class name method

--- app/models/english_region_general_election_party_performance.rb
+++ app/models/english_region_general_election_party_performance.rb
@@ -7,4 +7,10 @@ class EnglishRegionGeneralElectionPartyPerformance < ApplicationRecord
   def vote_share( valid_vote_count )
     ( ( self.cumulative_vote_count.to_f / valid_vote_count.to_f ) * 100 ).round( 1 )
   end
+  
+  def party_class_name
+    party_class_name = 'party'
+    party_class_name += ' ' + self.party_electoral_commission_id if self.party_electoral_commission_id
+    party_class_name
+  end
 end
