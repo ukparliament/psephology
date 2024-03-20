52f81bb26b26d8421f0062b1c4ab0bfe17485e0f
fantasticlife <michael@smethur.st:wq>
2024-03-20 17:19:25 +0000

    removed flag for child countries, added flag for ons link

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -143,7 +143,7 @@ create table countries (
 	id serial not null,
 	name varchar(255) not null,
 	geographic_code varchar(255),
-	directly_contains_constituency_areas boolean default false,
+	ons_linked boolean default false,
 	parent_country_id int,
 	constraint fk_parent_country foreign key (parent_country_id) references countries(id),
 	primary key (id)
