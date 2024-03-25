0219b62c9588fa04b70e71e8a2b1e141972c35e8
fantasticlife <michael@smethur.st:wq>
2024-03-25 11:17:54 +0000

    split out notional and real

--- app/controllers/general_election_vote_share_controller.rb
+++ app/controllers/general_election_vote_share_controller.rb
@@ -12,7 +12,15 @@ class GeneralElectionVoteShareController < ApplicationController
     ).first
     raise ActiveRecord::RecordNotFound unless @general_election
     @elections = @general_election.elections_by_vote_share
-    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by vote share"
-    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By vote share</span>".html_safe
+    
+    if @general_election.is_notional
+      @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by vote share"
+      @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By vote share</span>".html_safe
+      
+      render :template => 'general_election_vote_share/index_notional'
+    else
+      @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by vote share"
+      @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By vote share</span>".html_safe
+    end
   end
 end
