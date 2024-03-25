dea933f61d22965abc20ad400a7b540efe0ebe6c
fantasticlife <michael@smethur.st:wq>
2024-03-25 11:12:04 +0000

    split out notional and real

--- app/controllers/general_election_english_region_majority_controller.rb
+++ app/controllers/general_election_english_region_majority_controller.rb
@@ -19,7 +19,14 @@ class GeneralElectionEnglishRegionMajorityController < ApplicationController
   
     @elections = @general_election.elections_by_majority_in_english_region( @english_region )
     
-    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by majority"
-    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by majority</span>".html_safe
+    if @general_election.is_notional
+      @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by majority"
+      @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by majority</span>".html_safe
+      
+      render :template => 'general_election_english_region_majority/index_notional'
+    else
+      @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by majority"
+      @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by majority</span>".html_safe
+    end
   end
 end
