1beb54cf391b65299b99396bd904580fec53561b
fantasticlife <michael@smethur.st:wq>
2024-03-28 14:57:27 +0000

    removed meta url mapping action

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -24,19 +24,6 @@ class MetaController < ApplicationController
     @page_title = 'Database schema'
   end
   
-  def url_mapping
-    @page_title = 'URL mapping'
-    
-    @elections = Election.find_by_sql(
-      "
-        SELECT e.*, cg.name AS constituency_group_name
-        FROM elections e, constituency_groups cg
-        WHERE e.is_notional IS FALSE
-        AND e.constituency_group_id = cg.id
-      "
-    )
-  end
-  
   def redirect
     polling_on = params[:polling_on]
     constituency_name = params[:constituency]
