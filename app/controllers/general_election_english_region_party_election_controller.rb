4141127f142ad6c7a6116f8c2a9feb67eb03beae
fantasticlife <michael@smethur.st:wq>
2024-03-28 12:54:52 +0000

    made party performance tables at general election in an english region sortable for notionals and real

--- app/controllers/general_election_english_region_party_election_controller.rb
+++ app/controllers/general_election_english_region_party_election_controller.rb
@@ -23,6 +23,54 @@ class GeneralElectionEnglishRegionPartyElectionController < ApplicationControlle
     
     @elections_contested = @political_party.elections_contested_in_general_election_in_english_region( @general_election, @english_region )
     
+
+    
+    # Allow for table sorting.
+    @sort = params[:sort]
+    @order = params[:order]
+    if @order and @sort
+      case @order
+      when 'descending'
+        case @sort
+        when 'constituency-name'
+          @elections_contested.sort_by! {|election| election.constituency_name}.reverse!
+        when 'candidate-name'
+          @elections_contested.sort_by! {|election| election.winning_candidacy_candidate_family_name}.reverse!
+        when 'electorate'
+          @elections_contested.sort_by! {|election| election.electorate_population_count}.reverse!
+        when 'turnout'
+          @elections_contested.sort_by! {|election| election.turnout}.reverse!
+        when 'vote-count'
+          @elections_contested.sort_by! {|election| election.candidacy_vote_count}.reverse!
+        when 'vote-share'
+          @elections_contested.sort_by! {|election| election.candidacy_vote_share}.reverse!
+        when 'vote-change'
+          @elections_contested.sort_by! {|election| election.candidacy_vote_change || 0}.reverse!
+        when 'result-position'
+          @elections_contested.sort_by! {|election| election.candidacy_result_position}.reverse!
+        end
+      when 'ascending'
+        case @sort
+        when 'constituency-name'
+          @elections_contested.sort_by! {|election| election.constituency_name}
+        when 'candidate-name'
+          @elections_contested.sort_by! {|election| election.winning_candidacy_candidate_family_name}
+        when 'electorate'
+          @elections_contested.sort_by! {|election| election.electorate_population_count}
+        when 'turnout'
+          @elections_contested.sort_by! {|election| election.turnout}
+        when 'vote-count'
+          @elections_contested.sort_by! {|election| election.candidacy_vote_count}
+        when 'vote-share'
+          @elections_contested.sort_by! {|election| election.candidacy_vote_share}
+        when 'vote-change'
+          @elections_contested.sort_by! {|election| election.candidacy_vote_change || 0}
+        when 'result-position'
+          @elections_contested.sort_by! {|election| election.candidacy_result_position}
+        end
+      end
+    end
+    
     if @general_election.is_notional
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - Elections contested by #{@political_party.name}"
       @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - Elections contested by #{@political_party.name}</span>".html_safe
@@ -57,6 +105,52 @@ class GeneralElectionEnglishRegionPartyElectionController < ApplicationControlle
     @political_party = PoliticalParty.find( political_party )
     
     @elections_won = @political_party.elections_won_in_general_election_in_english_region( @general_election, @english_region )
+    
+    # Allow for table sorting.
+    @sort = params[:sort]
+    @order = params[:order]
+    if @order and @sort
+      case @order
+      when 'descending'
+        case @sort
+        when 'constituency-name'
+          @elections_won.sort_by! {|election| election.constituency_name}.reverse!
+        when 'candidate-name'
+          @elections_won.sort_by! {|election| election.winning_candidacy_candidate_family_name}.reverse!
+        when 'electorate'
+          @elections_won.sort_by! {|election| election.electorate_population_count}.reverse!
+        when 'turnout'
+          @elections_won.sort_by! {|election| election.turnout}.reverse!
+        when 'vote-count'
+          @elections_won.sort_by! {|election| election.winning_candidacy_vote_count}.reverse!
+        when 'vote-share'
+          @elections_won.sort_by! {|election| election.winning_candidacy_vote_share}.reverse!
+        when 'vote-change'
+          @elections_won.sort_by! {|election| election.winning_candidacy_vote_change || 0}.reverse!
+        when 'majority'
+          @elections_won.sort_by! {|election| election.majority}.reverse!
+        end
+      when 'ascending'
+        case @sort
+        when 'constituency-name'
+          @elections_won.sort_by! {|election| election.constituency_name}
+        when 'candidate-name'
+          @elections_won.sort_by! {|election| election.winning_candidacy_candidate_family_name}
+        when 'electorate'
+          @elections_won.sort_by! {|election| election.electorate_population_count}
+        when 'turnout'
+          @elections_won.sort_by! {|election| election.turnout}
+        when 'vote-count'
+          @elections_won.sort_by! {|election| election.winning_candidacy_vote_count}
+        when 'vote-share'
+          @elections_won.sort_by! {|election| election.winning_candidacy_vote_share}
+        when 'vote-change'
+          @elections_won.sort_by! {|election| election.winning_candidacy_vote_change || 0}
+        when 'majority'
+          @elections_won.sort_by! {|election| election.majority}
+        end
+      end
+    end
     
     if @general_election.is_notional
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - Elections won by #{@political_party.name}"
