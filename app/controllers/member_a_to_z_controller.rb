9a39767e229c89a3216a121db0e81ff438c74b38
fantasticlife <michael@smethur.st:wq>
2024-04-15 20:44:06 +0100

    basic controller and views

---
+++ app/controllers/member_a_to_z_controller.rb
@@ -0,0 +1,46 @@
+class MemberAToZController < ApplicationController
+  
+  def index
+    @letters = Member.find_by_sql(
+      "
+        SELECT UPPER( ( LEFT ( m.family_name, 1 ) ) ) AS letter
+        FROM members m, candidacies c
+        WHERE m.id = c.member_id
+        AND c.is_winning_candidacy IS TRUE
+        GROUP BY letter
+        ORDER BY letter;
+      "
+    )
+    @page_title = "Members - A to Z"
+    @multiline_page_title = "Members  <span class='subhead'>A to Z</span>".html_safe
+  end
+  
+  def show
+    @letters = Member.find_by_sql(
+      "
+        SELECT UPPER( ( LEFT ( m.family_name, 1 ) ) ) AS letter
+        FROM members m, candidacies c
+        WHERE m.id = c.member_id
+        AND c.is_winning_candidacy IS TRUE
+        GROUP BY letter
+        ORDER BY letter;
+      "
+    )
+    
+    letter = params[:letter]
+    @letter = letter.upcase
+    @page_title = "Members - #{@letter}"
+    @multiline_page_title = "Members  <span class='subhead'>Family name initial #{@letter}</span>".html_safe
+    @members = Member.find_by_sql(
+      "
+        SELECT m.*, count(c.id)
+        FROM members m, candidacies c
+        WHERE m.id = c.member_id
+        AND c.is_winning_candidacy IS TRUE
+        AND UPPER( LEFT( m.family_name, 1) ) = '#{@letter}'
+        GROUP BY m.id
+        ORDER BY family_name, given_name
+      "
+    )
+  end
+end
