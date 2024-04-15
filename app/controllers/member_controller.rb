5d28e3cdb9c26e22739818eaa94e6352c1f45686
fantasticlife <michael@smethur.st:wq>
2024-04-15 20:53:55 +0100

    get a-z letters for list

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -3,6 +3,16 @@ class MemberController < ApplicationController
   def index
     @page_title = 'Members'
     @members = Member.all.order( 'family_name' ).order( 'given_name' )
+    @letters = Member.find_by_sql(
+      "
+        SELECT UPPER( ( LEFT ( m.family_name, 1 ) ) ) AS letter
+        FROM members m, candidacies c
+        WHERE m.id = c.member_id
+        AND c.is_winning_candidacy IS TRUE
+        GROUP BY letter
+        ORDER BY letter;
+      "
+    )
     @members = Member.find_by_sql(
       "
         SELECT m.*, count(c.id)
