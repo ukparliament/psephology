ba0bd1a70a02d2bb504e870ae91e132bd987ff8c
Robert Brook <robertbrook@fastmail.fm>
2024-04-16 16:08:06 +0100

    added favicon, csv helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def csv_icon_link(url, title)
+		link_to( content_tag( 'span', 'CSV' ), url, :title => title, :class => 'csv' ) + content_tag( 'label', title )
+  end
+
 end
