83f410eadd957cd5f2d81098dd992f4bc2d79edf
fantasticlife <michael@smethur.st:wq>
2024-04-25 11:34:55 +0100

    more wrongly cased redirects

--- config/routes.rb
+++ config/routes.rb
@@ -163,5 +163,5 @@ Rails.application.routes.draw do
   get '/election/:polling_on/Statistics/Candidates' => 'meta#redirect_candidate_statistics', as: :meta_candidate_statistics
   get '/election/:polling_on/statistics/candidates' => 'meta#redirect_candidate_statistics', as: :meta_candidate_statistics2
   get '/election/:polling_on/Statistics/Majority' => 'meta#redirect_majority', as: :meta_majority
-  get '/election/:polling_on/Statistics/majority' => 'meta#redirect_majority', as: :meta_majority
+  get '/election/:polling_on/Statistics/majority' => 'meta#redirect_majority', as: :meta_majority2
 end
