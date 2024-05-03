3e15f7350884b298656ceaa900630e078e2357f9
fantasticlife <michael@smethur.st:wq>
2024-05-03 10:04:34 +0100

    added respond to for html and csv

--- app/controllers/general_election_majority_controller.rb
+++ app/controllers/general_election_majority_controller.rb
@@ -18,12 +18,20 @@ class GeneralElectionMajorityController < ApplicationController
       
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by majority"
       @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By majority</span>".html_safe
-      
-      render :template => 'general_election_majority/index_notional'
+    
     else
     
       @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by majority"
       @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By majority</span>".html_safe
+    end
+    
+    respond_to do |format|
+      format.csv {
+        response.headers['Content-Disposition'] = "attachment; filename=\"winning-majorities-#{'notional-' if @general_election.is_notional}general-election-#{@general_election.polling_on.strftime( '%d-%m-%Y' )}.csv\""
+      }
+      format.html{
+        render :template => 'general_election_majority/index_notional' if @general_election.is_notional
+      }
     end
   end
 end
