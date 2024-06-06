c86ab42b69dd3cbe183f3e19bbff1c36fc93a067
fantasticlife <michael@smethur.st>
2024-06-06 13:44:28 +0100

    added respond to

--- app/controllers/general_election_country_uncertified_candidacy_controller.rb
+++ app/controllers/general_election_country_uncertified_candidacy_controller.rb
@@ -19,5 +19,13 @@ class GeneralElectionCountryUncertifiedCandidacyController < ApplicationControll
     
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - uncertified candidacies"
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - uncertified candidacies</span>".html_safe
+    
+    respond_to do |format|
+      format.csv {
+        response.headers['Content-Disposition'] = "attachment; filename=\"non-party-candidates-in-#{@country.name.downcase.gsub( ' ', '-')}-#{'notional-' if @general_election.is_notional}general-election-#{@general_election.polling_on.strftime( '%d-%m-%Y' )}.csv\""
+        render :template => 'general_election_uncertified_candidacy/index'
+      }
+      format.html
+    end
   end
 end
