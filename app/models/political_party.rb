0cfa0aed65bd3f61977406a7ac4ac5ce1f5dc27a
fantasticlife <michael@smethur.st>
2024-06-06 15:35:58 +0100

    elections contested in general election in country method updated to return constituency area id and geographic code

--- app/models/political_party.rb
+++ app/models/political_party.rb
@@ -293,6 +293,8 @@ class PoliticalParty < ApplicationRecord
       "
         SELECT e.*,
           constituency_group.name AS constituency_name,
+          constituency_group.constituency_area_id AS constituency_area_id,
+          constituency_area.geographic_code AS constituency_area_geographic_code,
           candidacy.candidate_given_name AS winning_candidacy_candidate_given_name,
           candidacy.candidate_family_name AS winning_candidacy_candidate_family_name,
           candidacy.vote_count AS candidacy_vote_count,
@@ -324,6 +326,12 @@ class PoliticalParty < ApplicationRecord
           )
         ) constituency_group
         ON constituency_group.id = e.constituency_group_id
+        
+        LEFT JOIN (
+          SELECT *
+          FROM constituency_areas
+        ) constituency_area
+        ON constituency_area.id = constituency_group.constituency_area_id
       
         LEFT JOIN (
           SELECT m.*, can.election_id AS election_id
