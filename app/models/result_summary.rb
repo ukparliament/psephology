c7c8ebbfb73b276626d68835da1bd11c8343ace1
fantasticlife <michael@smethur.st>
2024-06-07 15:41:54 +0100

    added is gain method

--- app/models/result_summary.rb
+++ app/models/result_summary.rb
@@ -7,4 +7,10 @@ class ResultSummary < ApplicationRecord
   def to_political_party
     PoliticalParty.find( self.to_political_party_id )
   end
+  
+  def is_gain?
+    is_gain = false
+    is_gain = true if self.short_summary.include?( ' gain from ' )
+    is_gain
+  end
 end
