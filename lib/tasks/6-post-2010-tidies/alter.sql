8b4a89d3276f07382cd42d42a85bd78441431095
fantasticlife <michael@smethur.st>
2024-06-17 12:54:43 +0100

    tweaked labour party disclaimer

--- lib/tasks/6-post-2010-tidies/alter.sql
+++ lib/tasks/6-post-2010-tidies/alter.sql
@@ -1,6 +1,6 @@
 /* Add Labour / Co-op disclaimer to the political parties table */
 alter table political_parties add column disclaimer varchar(500);
-update political_parties set disclaimer = 'Figures presented for Labour include candidates certified by both the Labour and the Co-operative parties.' where id = 3;
+update political_parties set disclaimer = 'Figures presented for Labour include those candidates certified by both the Labour and the Co-operative parties.' where id = 3;
 
 /* Add Democracy Club person ID to the candidacies table */
 alter table candidacies add column democracy_club_person_identifier int;
