755d0440aa5f9889749453ca96c0f26d17727d2b
fantasticlife <michael@smethur.st>
2024-06-28 19:33:45 +0100

    added section, subsection, csv, crumb and description

--- app/controllers/general_election_party_controller.rb
+++ app/controllers/general_election_party_controller.rb
@@ -14,13 +14,25 @@ class GeneralElectionPartyController < ApplicationController
     
     @party_performances = @general_election.party_performance
     
+    @section = 'general-elections'
+    @subsection = 'parties'
+    @csv_url = general_election_party_list_url( :format => 'csv' )
+    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
+    
     if @general_election.is_notional
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by party"
       @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By party</span>".html_safe
     
+    
+      @description = ""
+      @crumb += "<li>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} (Notional)</li>"
+    
     else
       @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by party"
       @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By party</span>".html_safe
+      
+      @description = ""
+      @crumb += "<li>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</li>"
     end
     
     respond_to do |format|
@@ -48,14 +60,25 @@ class GeneralElectionPartyController < ApplicationController
     political_party = params[:political_party]
     @political_party = PoliticalParty.find( political_party )
     
+    @section = 'general-elections'
+    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
+    
     if @general_election.is_notional
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@political_party.name}"
       @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@political_party.name}</span>".html_safe
+      
+      @description = ""
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} (Notional)</a></li>"
+      @crumb += "<li>#{@political_party.name}</li>"
       
       render :template => 'general_election_party/show_notional'
     else
       @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@political_party.name}"
       @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@political_party.name}</span>".html_safe
+      
+      @description = ""
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</a></li>"
+      @crumb += "<li>#{@political_party.name}</li>"
     end
   end
 end
