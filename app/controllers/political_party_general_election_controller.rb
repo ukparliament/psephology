ef97128d5a592008ca39c1da7995415acd454189
fantasticlife <michael@smethur.st>
2024-06-28 14:54:17 +0100

    section, descripton, csv and crumb

--- app/controllers/political_party_general_election_controller.rb
+++ app/controllers/political_party_general_election_controller.rb
@@ -7,5 +7,11 @@ class PoliticalPartyGeneralElectionController < ApplicationController
     @multiline_page_title = "#{@political_party.name} <span class='subhead'>General elections</span>".html_safe
     
     @general_elections = @political_party.general_elections
+    
+    @section = 'political-parties'
+    @description = "#{@political_party.name} - General elections."
+    @crumb = "<li><a href='/political-parties/winning'>Political parties</a></li>"
+    @crumb += "<li><a href='/political-parties/#{@political_party.id}'>#{@political_party.name}</a></li>"
+    @crumb += "<li>General elections</li>"
   end
 end
