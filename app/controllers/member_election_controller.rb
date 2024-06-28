419d507643d06be29cf5bc0001634ee49eaf5e87
fantasticlife <michael@smethur.st>
2024-06-28 14:31:27 +0100

    section, descripton, csv and crumb

--- app/controllers/member_election_controller.rb
+++ app/controllers/member_election_controller.rb
@@ -4,9 +4,16 @@ class MemberElectionController < ApplicationController
     member = params[:member]
     @member = Member.find_by_mnis_id( member )
     raise ActiveRecord::RecordNotFound unless @member
-    @page_title = "#{@member.display_name} - All elections contested"
-    @multiline_page_title = "#{@member.display_name}  <span class='subhead'>All elections contested</span>".html_safe
+    @page_title = "#{@member.display_name} - Elections contested"
+    @multiline_page_title = "#{@member.display_name}  <span class='subhead'>Elections contested</span>".html_safe
     @elections = @member.elections
+    
+    @section = 'members'
+    @subsection = 'contested'
+    @description = "Elections contested by #{@member.display_name}."
+    @crumb = "<li><a href='/members'>Members</a></li>"
+    @crumb += "<li><a href='/members/#{@member.mnis_id}'>#{@member.display_name}</a></li>"
+    @crumb += "<li>Elections contested</li>"
   end
   
   def won
@@ -16,5 +23,12 @@ class MemberElectionController < ApplicationController
     @page_title = "#{@member.display_name} - Elections won"
     @multiline_page_title = "#{@member.display_name}  <span class='subhead'>Elections won</span>".html_safe
     @elections_won = @member.elections_won
+    
+    @section = 'members'
+    @subsection = 'won'
+    @description = "Elections won by #{@member.display_name}."
+    @crumb = "<li><a href='/members'>Members</a></li>"
+    @crumb += "<li><a href='/members/#{@member.mnis_id}'>#{@member.display_name}</a></li>"
+    @crumb += "<li>Elections won</li>"
   end
 end
