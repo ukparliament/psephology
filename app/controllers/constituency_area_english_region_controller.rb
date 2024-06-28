fca9e0e34d5f28d35869311aea4c44755afaad9e
fantasticlife <michael@smethur.st>
2024-06-28 18:04:09 +0100

    section, descriptions and crumb added

--- app/controllers/constituency_area_english_region_controller.rb
+++ app/controllers/constituency_area_english_region_controller.rb
@@ -7,6 +7,12 @@ class ConstituencyAreaEnglishRegionController < ApplicationController
     @multiline_page_title = "Current constituencies <span class='subhead'>English regions</span>".html_safe
     @english_regions = @country.current_english_regions
     raise ActiveRecord::RecordNotFound if @english_regions.empty?
+    
+    @section = 'constituency-areas'
+    @description = ""
+    @crumb = "<li><a href='/constituency-areas/current'>Constituency areas</a></li>"
+    @crumb += "<li><a href='/constituency-areas/current/countries/#{@country.id}'>#{@country.name}</a></li>"
+    @crumb += "<li>Regions</li>"
   end
   
   def show
@@ -18,5 +24,11 @@ class ConstituencyAreaEnglishRegionController < ApplicationController
     @multiline_page_title = "Current constituencies <span class='subhead'>#{@english_region.name}, #{@country.name}</span>".html_safe
     @current_constituencies = @country.current_constituencies_in_region( @english_region )
     raise ActiveRecord::RecordNotFound if @current_constituencies.empty?
+    
+    @section = 'constituency-areas'
+    @description = ""
+    @crumb = "<li><a href='/constituency-areas/current'>Constituency areas</a></li>"
+    @crumb += "<li><a href='/constituency-areas/current/countries/#{@country.id}'>#{@country.name}</a></li>"
+    @crumb += "<li>#{@english_region.name}</li>"
   end
 end
