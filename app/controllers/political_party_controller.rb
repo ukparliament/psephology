097f9b372bb8b49077cce2404f262f44b89f9855
fantasticlife <michael@smethur.st>
2024-06-28 14:54:00 +0100

    section, descripton, csv and crumb

--- app/controllers/political_party_controller.rb
+++ app/controllers/political_party_controller.rb
@@ -12,11 +12,22 @@ class PoliticalPartyController < ApplicationController
         ORDER BY pp.name
       "
     )
+    
+    @section = 'political-parties'
+    @subsection = 'contested'
+    @description = "Political parties certifying candidates in elections to the Parliament of the United Kingdom since 2010."
+    @crumb = "<li><a href='/political-parties/winning'>Political parties</a></li>"
+    @crumb += "<li>Contesting parties</li>"
   end
   
   def winning
     @page_title = 'Winning political parties'
     @political_parties = PoliticalParty.all.where( 'has_been_parliamentary_party IS TRUE' ).order( 'name' )
+    
+    @section = 'political-parties'
+    @subsection = 'won'
+    @description = "Political parties certifying candidates in elections to the Parliament of the United Kingdom since 2010, where one or more of those candidates have won an election."
+    @crumb = "<li>Political parties</li>"
   end
   
   def fall
@@ -32,6 +43,11 @@ class PoliticalPartyController < ApplicationController
     numbers = [1,2,3].each do |number|
       @fall += @words[rand( 0 .. @words.length - 1 )] + ' '
     end
+    
+    @section = 'political-parties'
+    @description = "Fall b-sides."
+    @crumb = "<li><a href='/political-parties/winning'>Political parties</a></li>"
+    @crumb += "<li>Fall b-sides</li>"
   end
   
   def show
@@ -40,5 +56,10 @@ class PoliticalPartyController < ApplicationController
     @page_title = @political_party.name
     
     @general_elections = @political_party.general_elections
+    
+    @section = 'political-parties'
+    @description = "#{@political_party.name}."
+    @crumb = "<li><a href='/political-parties/winning'>Political parties</a></li>"
+    @crumb += "<li>#{@political_party.name}</li>"
   end
 end
