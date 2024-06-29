ebece29baa57ec4e3bef477f9dbadc23008dcf95
fantasticlife <michael@smethur.st>
2024-06-29 10:45:20 +0100

    added section, subsection, csv url, description and crumb

--- app/controllers/general_election_country_turnout_controller.rb
+++ app/controllers/general_election_country_turnout_controller.rb
@@ -17,17 +17,32 @@ class GeneralElectionCountryTurnoutController < ApplicationController
     
     @elections = @general_election.elections_by_turnout_in_country( @country )
     
+    @section = 'general-elections'
+    @subsection = 'turnouts'
+    @csv_url = general_election_country_turnout_list_url( :format => 'csv' )
+    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
+    
     if @general_election.is_notional
       
       @page_title = "Notional results for a UK general election on  #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - by turnout"
     
       @multiline_page_title = "Notional results for a UK general election on  #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - by turnout</span>".html_safe
       
+      @description = ""
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} (Notional)</a></li>"
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/countries/#{@country.id}/political-parties'>#{@country.name}</a></li>"
+      @crumb += "<li>Turnout</li>"
+      
     else
     
       @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - by turnout"
     
       @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - by turnout</span>".html_safe
+      
+      @description = ""
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</a></li>"
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/countries/#{@country.id}/political-parties'>#{@country.name}</a></li>"
+      @crumb += "<li>Turnout</li>"
     end
     
     respond_to do |format|
