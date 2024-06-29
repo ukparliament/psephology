f4a95938631373fb101e0d58468885b2a4d3a88b
fantasticlife <michael@smethur.st>
2024-06-29 11:00:50 +0100

    added section, subsection, csv url, description and crumb

--- app/controllers/general_election_country_declaration_time_controller.rb
+++ app/controllers/general_election_country_declaration_time_controller.rb
@@ -20,6 +20,14 @@ class GeneralElectionCountryDeclarationTimeController < ApplicationController
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - by declaration time"
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - by declaration time</span>".html_safe
     
+    @section = 'general-elections'
+    @subsection = 'declaration-times'
+    @csv_url = general_election_country_declaration_time_list_url( :format => 'csv' )
+    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
+    @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</a></li>"
+    @crumb += "<li><a href='/general-elections/#{@general_election.id}/countries/#{@country.id}/political-parties'>#{@country.name}</a></li>"
+    @crumb += "<li>Declaration times</li>"
+    
     respond_to do |format|
       format.csv {
         response.headers['Content-Disposition'] = "attachment; filename=\"declaration-times-in-#{@country.name.downcase.gsub( ' ', '-')}-#{'notional-' if @general_election.is_notional}general-election-#{@general_election.polling_on.strftime( '%d-%m-%Y' )}.csv\""
