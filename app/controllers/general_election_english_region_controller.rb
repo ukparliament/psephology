d1e8e114ef830f248d00ad575288623d21cdd76c
fantasticlife <michael@smethur.st>
2024-06-30 12:08:22 +0100

    added section, description and crumb

--- app/controllers/general_election_english_region_controller.rb
+++ app/controllers/general_election_english_region_controller.rb
@@ -9,14 +9,27 @@ class GeneralElectionEnglishRegionController < ApplicationController
     
     @english_regions = @general_election.english_regions_in_country( @country )
     
+    @section = 'general-elections'
+    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
+    
     if @general_election.is_notional
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - English regions"
       @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>English regions</span>".html_safe
+      
+      @description = ""
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} (Notional)</a></li>"
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/countries/#{@country.id}/political-parties'>#{@country.name}</a></li>"
+      @crumb += "<li>Regions</li>"
       
       render :template => 'general_election_english_region/index_notional'
     else
       @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - English regions"
       @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>English regions</span>".html_safe
+      
+      @description = ""
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</a></li>"
+      @crumb += "<li><a href='/general-elections/#{@general_election.id}/countries/#{@country.id}/political-parties'>#{@country.name}</a></li>"
+      @crumb += "<li>Regions</li>"
     end
   end
   
