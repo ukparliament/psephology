c7badbf949bea394a83f6dc8178dc6731922b6a3
fantasticlife <michael@smethur.st>
2024-07-01 11:34:33 +0100

    added descriptions

--- app/controllers/general_election_controller.rb
+++ app/controllers/general_election_controller.rb
@@ -24,17 +24,16 @@ class GeneralElectionController < ApplicationController
       "
     )
     
-    @page_title = 'General elections'
-    
     respond_to do |format|
       format.csv {
         @real_and_notional_general_elections = @general_elections.concat( @notional_general_elections )
       }
       format.html {
-        @section = 'general-elections'
-        @description = ""
+        @page_title = 'General elections'
+        @description = 'General elections to the Parliament of the United Kingdom.'
         @csv_url = general_election_list_url( :format => 'csv' )
         @crumb = "<li>General elections</li>"
+        @section = 'general-elections'
       }
     end
   end
@@ -54,26 +53,27 @@ class GeneralElectionController < ApplicationController
     @countries = @general_election.top_level_countries_with_elections
     @elections = @general_election.elections
     
+    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
     @section = 'general-elections'
     @subsection = 'constituency-areas'
-    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
+    
     
     if @general_election.is_notional
       
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by constituency"
       @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By constituency</span>".html_safe
       
-      @description = ""
+      @description = "Notional results for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} listed by constituency."
       @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} (Notional)</a></li>"
       @crumb += "<li>Constituencies</li>"
       
       render :template => 'general_election/show_notional'
     else
     
-      @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by constituency"
-      @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By constituency</span>".html_safe
+      @page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by constituency"
+      @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By constituency</span>".html_safe
 
-      @description = ""
+      @description = "Results for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} listed by constituency."
       @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</a></li>"
       @crumb += "<li>Constituencies</li>"
     end
