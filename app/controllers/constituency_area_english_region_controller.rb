70b0b8459406d69636e731c930ad20d9734e09a6
fantasticlife <michael@smethur.st>
2024-07-01 10:31:37 +0100

    added descriptions

--- app/controllers/constituency_area_english_region_controller.rb
+++ app/controllers/constituency_area_english_region_controller.rb
@@ -3,16 +3,16 @@ class ConstituencyAreaEnglishRegionController < ApplicationController
   def index
     country = params[:country]
     @country = Country.find( country )
-    @page_title = "Current constituencies - English regions"
-    @multiline_page_title = "Current constituencies <span class='subhead'>English regions</span>".html_safe
     @english_regions = @country.current_english_regions
     raise ActiveRecord::RecordNotFound if @english_regions.empty?
     
-    @section = 'constituency-areas'
-    @description = ""
+    @page_title = "Current constituencies - English regions"
+    @multiline_page_title = "Current constituencies <span class='subhead'>English regions</span>".html_safe
+    @description = "Current constituency areas in #{@country.name}, by region."
     @crumb = "<li><a href='/constituency-areas/current'>Constituency areas</a></li>"
     @crumb += "<li><a href='/constituency-areas/current/countries/#{@country.id}'>#{@country.name}</a></li>"
     @crumb += "<li>Regions</li>"
+    @section = 'constituency-areas'
   end
   
   def show
@@ -20,15 +20,16 @@ class ConstituencyAreaEnglishRegionController < ApplicationController
     @country = Country.find( country )
     english_region = params[:english_region]
     @english_region = EnglishRegion.find( english_region )
-    @page_title = "Current constituencies - #{@english_region.name}, #{@country.name}"
-    @multiline_page_title = "Current constituencies <span class='subhead'>#{@english_region.name}, #{@country.name}</span>".html_safe
     @current_constituencies = @country.current_constituencies_in_region( @english_region )
     raise ActiveRecord::RecordNotFound if @current_constituencies.empty?
     
-    @section = 'constituency-areas'
-    @description = ""
+
+    @page_title = "Current constituencies - #{@english_region.name}, #{@country.name}"
+    @multiline_page_title = "Current constituencies <span class='subhead'>#{@english_region.name}, #{@country.name}</span>".html_safe
+    @description = "Current constituency areas in #{@english_region.name}, England."
     @crumb = "<li><a href='/constituency-areas/current'>Constituency areas</a></li>"
     @crumb += "<li><a href='/constituency-areas/current/countries/#{@country.id}'>#{@country.name}</a></li>"
     @crumb += "<li>#{@english_region.name}</li>"
+    @section = 'constituency-areas'
   end
 end
