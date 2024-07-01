f4bd6bda5a2c5968334d7afd433b8e96f1b530cc
fantasticlife <michael@smethur.st>
2024-07-01 12:07:59 +0100

    added descriptions

--- app/controllers/general_election_declaration_times_controller.rb
+++ app/controllers/general_election_declaration_times_controller.rb
@@ -13,15 +13,15 @@ class GeneralElectionDeclarationTimesController < ApplicationController
     raise ActiveRecord::RecordNotFound unless @general_election
   
     @elections = @general_election.elections_by_declaration_time
-    @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by declaration time"
-    @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By declaration time</span>".html_safe
-    
-    @section = 'general-elections'
-    @subsection = 'declaration-times'
+    @page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - by declaration time"
+    @multiline_page_title = "Results for a  UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>By declaration time</span>".html_safe
+    @description = "Results for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} listed by declaration times."
     @csv_url = general_election_declaration_time_list_url( :format => 'csv' )
     @crumb = "<li><a href='/general-elections'>General elections</a></li>"
     @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</a></li>"
     @crumb += "<li>Declaration times</li>"
+    @section = 'general-elections'
+    @subsection = 'declaration-times'
     
     respond_to do |format|
       format.csv {
