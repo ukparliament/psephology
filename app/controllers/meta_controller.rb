2a81ef00069e861dae90308cc3a764f68f841143
fantasticlife <michael@smethur.st>
2024-07-01 10:38:04 +0100

    added descriptions

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -2,37 +2,37 @@ class MetaController < ApplicationController
   
   def index
     @page_title = 'Meta'
-    @description = 'Meta.'
+    @description = 'Information about this website.'
     @crumb = '<li>Meta</li>'
   end
   
   def coverage
     @page_title = 'Coverage: June 2024'
-    @description = 'Content coverage.'
+    @description = 'Elections to the United Kingdom Parliament covered by this website.'
     @crumb = '<li>Coverage</li>'
   end
   
   def roadmap
     @page_title = 'Roadmap'
-    @description = 'Our roadmap.'
+    @description = 'The roadmap for development of this website.'
     @crumb = '<li>Roadmap</li>'
   end
   
   def contact
     @page_title = 'Contact'
-    @description = 'Contact us.'
+    @description = 'How to contact us.'
     @crumb = '<li>Contact</li>'
   end
   
   def cookies
     @page_title = 'Cookies'
-    @description = 'Cookie policy.'
+    @description = 'The cookie policy for this website.'
     @crumb = '<li>Cookies</li>'
   end
   
   def schema
     @page_title = 'Database schema'
-    @description = 'Database schema.'
+    @description = 'An entity relationship diagram describing the database schema for this website.'
     @crumb = '<li>Database schema</li>'
   end
 end
