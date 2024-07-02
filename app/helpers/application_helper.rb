4ea4b82cc9d171a89740764193c510539fcbf5d8
James Jefferies <james@shedcode.co.uk>
2024-07-02 19:46:15 +0100

    Making the 2019 hardcoding a bit more configurable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,29 @@ module ApplicationHelper
 		link_to( content_tag( 'span', 'CSV' ), url, :title => title, :class => 'csv' ) + content_tag( 'label', title )
   end
 
+  def election_lists_of_links
+    content = ""
+
+    general_election_links = relevant_general_elections.pluck(:id, :polling_on).map do |id, polling_on|
+      link_to(polling_on.year, general_election_party_list_path(id))
+    end
+  end
+
+  def output_election_lists_of_links
+    sanitize election_lists_of_links.to_sentence
+  end
+
+  def output_election_years
+    relevant_general_elections.pluck(:polling_on).map do |polling_on|
+      polling_on.year
+    end.to_sentence(last_word_connector: ' or ')
+  end
+
+  def relevant_general_elections
+    GeneralElection.where(is_notional: false).where("EXTRACT(YEAR FROM polling_on) <= #{end_year}").order(:polling_on)
+  end
+
+  def end_year
+    ENV.fetch('LATEST_ELECTION_YEAR', 2019)
+  end
 end
