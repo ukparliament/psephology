5d9c4e2611340f6f227e453313ab7a3f9696aa12
fantasticlife <michael@smethur.st>
2024-07-02 12:02:56 +0100

    added crumb, page title, multiline page title and description

--- app/controllers/general_election_country_candidacy_controller.rb
+++ app/controllers/general_election_country_candidacy_controller.rb
@@ -17,6 +17,27 @@ class GeneralElectionCountryCandidacyController < ApplicationController
         @section = 'general-elections'
         @csv_url = general_election_country_candidacy_list_url( :format => 'csv' )
         @description = ''
+        @crumb = "<li><a href='/general-elections'>General elections</a></li>"
+        
+        @crumb = "<li><a href='/general-elections'>General elections</a></li>"
+        if @general_election.is_notional
+          @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - candidates"
+          @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - candidates</span>".html_safe
+          @description = "Notional results in #{@country.name} for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} listing all candidates, available as a CSV."
+          @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} (Notional)</a></li>"
+          @crumb += "<li><a href='/general-elections/#{@general_election.id}/countries/#{@country.id}/political-parties'>#{@country.name}</a></li>"
+          @crumb += "<li>Candidates</li>"
+        else
+          @page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@country.name} - candidates"
+          @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@country.name} - candidates</span>".html_safe
+          @description = "Results in #{@country.name} for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} listing all candidates, available as a CSV."
+          @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</a></li>"
+          @crumb += "<li><a href='/general-elections/#{@general_election.id}/countries/#{@country.id}/political-parties'>#{@country.name}</a></li>"
+          @crumb += "<li>Candidates</li>"
+        end
+        
+        @csv_url = general_election_candidacy_list_url( :format => 'csv' )
+        @section = 'general-elections'
       }
     end
   end
