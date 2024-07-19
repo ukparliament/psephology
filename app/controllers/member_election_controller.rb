e5893b4bbe45cbaae663333193572af14179e49a
fantasticlife <michael@smethur.st>
2024-07-19 08:05:13 +0100

    crumb trail constructed as array

--- app/controllers/member_election_controller.rb
+++ app/controllers/member_election_controller.rb
@@ -4,31 +4,33 @@ class MemberElectionController < ApplicationController
     member = params[:member]
     @member = Member.find_by_mnis_id( member )
     raise ActiveRecord::RecordNotFound unless @member
-    @page_title = "#{@member.display_name} - Elections contested"
-    @multiline_page_title = "#{@member.display_name}  <span class='subhead'>Elections contested</span>".html_safe
+    
     @elections = @member.elections
     
+    @page_title = "#{@member.display_name} - Elections contested"
+    @multiline_page_title = "#{@member.display_name}  <span class='subhead'>Elections contested</span>".html_safe
+    @description = "Elections contested by #{@member.display_name}."
+    @crumb << { label: 'Members', url: member_list_url }
+    @crumb << { label: @member.display_name, url: member_show_url( :member => @member.mnis_id ) }
+    @crumb << { label: 'Elections contested', url: nil }
     @section = 'members'
     @subsection = 'contested'
-    @description = "Elections contested by #{@member.display_name}."
-    @crumb = "<li><a href='/members'>Members</a></li>"
-    @crumb += "<li><a href='/members/#{@member.mnis_id}'>#{@member.display_name}</a></li>"
-    @crumb += "<li>Elections contested</li>"
   end
   
   def won
     member = params[:member]
     @member = Member.find_by_mnis_id( member )
     raise ActiveRecord::RecordNotFound unless @member
-    @page_title = "#{@member.display_name} - Elections won"
-    @multiline_page_title = "#{@member.display_name}  <span class='subhead'>Elections won</span>".html_safe
+    
     @elections_won = @member.elections_won
     
+    @page_title = "#{@member.display_name} - Elections won"
+    @multiline_page_title = "#{@member.display_name}  <span class='subhead'>Elections won</span>".html_safe
+    @description = "Elections won by #{@member.display_name}."
+    @crumb << { label: 'Members', url: member_list_url }
+    @crumb << { label: @member.display_name, url: member_show_url( :member => @member.mnis_id ) }
+    @crumb << { label: 'Elections won', url: nil }
     @section = 'members'
     @subsection = 'won'
-    @description = "Elections won by #{@member.display_name}."
-    @crumb = "<li><a href='/members'>Members</a></li>"
-    @crumb += "<li><a href='/members/#{@member.mnis_id}'>#{@member.display_name}</a></li>"
-    @crumb += "<li>Elections won</li>"
   end
 end
