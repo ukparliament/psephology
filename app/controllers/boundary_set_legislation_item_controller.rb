4d9019945b8394358a6a6bda6950a3cef6ea4fd3
fantasticlife <michael@smethur.st>
2024-07-19 08:18:06 +0100

    crumb trail constructed as array

--- app/controllers/boundary_set_legislation_item_controller.rb
+++ app/controllers/boundary_set_legislation_item_controller.rb
@@ -18,12 +18,11 @@ class BoundarySetLegislationItemController < ApplicationController
     
     @page_title = "Boundary set for #{@boundary_set.display_title} - establishing legislation"
     @multiline_page_title = "Boundary set for #{@boundary_set.display_title} <span class='subhead'>Establishing legislation</span>".html_safe
-    
+    @description = "Legislation establishing the #{@boundary_set.display_title} boundary set."
+    @crumb << { label: 'Boundary sets', url: boundary_set_list_url }
+    @crumb << { label: @boundary_set.display_title, url: boundary_set_show_url( :boundary_set => @boundary_set ) }
+    @crumb << { label: 'Establishing legislation', url: nil }
     @section = 'boundary-sets'
     @subsection = 'establishing-legislation'
-    @description = "Legislation establishing the #{@boundary_set.display_title} boundary set."
-    @crumb = "<li><a href='/boundary-sets'>Boundary sets</a></li>"
-    @crumb += "<li><a href='/boundary-sets/#{@boundary_set.id}'>" + @boundary_set.display_title + '</a></li>'
-    @crumb += '<li>Establishing legislation</li>'
   end
 end
