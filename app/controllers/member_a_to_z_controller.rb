be91c1077fa05b16690e7899947c904bb250e266
fantasticlife <michael@smethur.st>
2024-07-19 08:05:48 +0100

    crumb trail constructed as array

--- app/controllers/member_a_to_z_controller.rb
+++ app/controllers/member_a_to_z_controller.rb
@@ -11,14 +11,14 @@ class MemberAToZController < ApplicationController
         ORDER BY letter;
       "
     )
+    
     @page_title = "Members - A to Z"
     @multiline_page_title = "Members <span class='subhead'>A to Z</span>".html_safe
-    
-    @section = 'members'
     @description = "Winning candidates in elections to the Parliament of the United Kingdom since 2010, listed alphabetically by family name."
     @csv_url = member_list_url( :format => 'csv' )
-    @crumb = "<li><a href='/members'>Members</a></li>"
-    @crumb += '<li>A to Z</li>'
+    @crumb << { label: 'Members', url: member_list_url }
+    @crumb << { label: 'A to Z', url: nil }
+    @section = 'members'
   end
   
   def show
@@ -35,8 +35,6 @@ class MemberAToZController < ApplicationController
     
     letter = params[:letter]
     @letter = letter.upcase
-    @page_title = "Members - #{@letter}"
-    @multiline_page_title = "Members <span class='subhead'>By family name - #{@letter}</span>".html_safe
     
     @members = Member.find_by_sql(
       "
@@ -52,11 +50,13 @@ class MemberAToZController < ApplicationController
 
     raise ActiveRecord::RecordNotFound if @members.empty?
     
-    @section = 'members'
-    @subsection = @letter.downcase
+    @page_title = "Members - #{@letter}"
+    @multiline_page_title = "Members <span class='subhead'>By family name - #{@letter}</span>".html_safe
     @description = "Winning candidates in elections to the Parliament of the United Kingdom since 2010, listed alphabetically by family name, family name beginning #{@letter}."
     @csv_url = member_list_url( :format => 'csv' )
-    @crumb = "<li><a href='/members'>Members</a></li>"
-    @crumb += "<li>#{@letter}</li>"
+    @crumb << { label: 'Members', url: member_list_url }
+    @crumb << { label: @letter, url: nil }
+    @section = 'members'
+    @subsection = @letter.downcase
   end
 end
