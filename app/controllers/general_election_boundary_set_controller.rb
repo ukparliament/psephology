6afb00fd4c3432b3636831f302e30c56dcd46100
fantasticlife <michael@smethur.st>
2024-07-19 08:15:32 +0100

    crumb trail constructed as array

--- app/controllers/general_election_boundary_set_controller.rb
+++ app/controllers/general_election_boundary_set_controller.rb
@@ -18,11 +18,10 @@ class GeneralElectionBoundarySetController < ApplicationController
     @page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - boundary sets"
     @multiline_page_title = "UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>Boundary sets</span>".html_safe
     @description = "Boundary sets in operation for the general election to the Parliament of the United Kingdom on #{@general_election.crumb_label}."
-    
     @csv_url = general_election_boundary_set_list_url( :format => 'csv' )
-    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
-    @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}</a></li>"
-    @crumb += "<li>Boundary sets</li>"
+    @crumb << { label: 'General elections', url: general_election_list_url }
+    @crumb << { label: @general_election.crumb_label, url: general_election_party_list_url }
+    @crumb << { label: 'Boundary sets', url: nil }
     @section = 'general-elections'
     @subsection = 'boundary-sets'
     
