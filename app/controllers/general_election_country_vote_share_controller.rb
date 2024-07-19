275fdeec6e1441902bd5cb3d5ce4251e70241b0b
fantasticlife <michael@smethur.st>
2024-07-19 08:12:29 +0100

    crumb trail constructed as array

--- app/controllers/general_election_country_vote_share_controller.rb
+++ app/controllers/general_election_country_vote_share_controller.rb
@@ -17,11 +17,10 @@ class GeneralElectionCountryVoteShareController < ApplicationController
     
     @elections = @general_election.elections_by_vote_share_in_country( @country )
     
-    @csv_url = general_election_country_vote_share_list_url( :format => 'csv' )
-    @crumb = "<li><a href='/general-elections'>General elections</a></li>"
-    @crumb += "<li><a href='/general-elections/#{@general_election.id}/political-parties'>#{@general_election.crumb_label}</a></li>"
-    @crumb += "<li><a href='/general-elections/#{@general_election.id}/countries/#{@country.id}/political-parties'>#{@country.name}</a></li>"
-    @crumb += "<li>Vote shares</li>"
+    @crumb << { label: 'General elections', url: general_election_list_url }
+    @crumb << { label: @general_election.crumb_label, url: general_election_party_list_url }
+    @crumb << { label: @country.name, url: general_election_country_political_party_list_url }
+    @crumb << { label: 'Vote shares', url: nil }
     @section = 'general-elections'
     @subsection = 'vote-shares'
     
