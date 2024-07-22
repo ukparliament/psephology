3c60aa0e19bae8f0cb9309544a5479a9be73dc2e
fantasticlife <michael@smethur.st>
2024-07-22 21:00:05 +0100

    bexley electorate corrections:

---
+++ lib/tasks/test/8-alter-electorates/alter.sql
@@ -0,0 +1,5 @@
+/* Bexleyheath and Crayford */
+update electorates set population_count = 2 where id = 70297;
+	
+/* Old Bexley and Sidcup */
+update electorates set population_count = 4 where id = 3688;
