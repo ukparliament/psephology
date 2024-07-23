d0142765dd0c46d3b27acbe64483e1ae1bf991ec
fantasticlife <michael@smethur.st>
2024-07-23 10:42:34 +0100

    moved to live

---
+++ lib/tasks/live/8-alter-electorates/alter.sql
@@ -0,0 +1,5 @@
+/* Bexleyheath and Crayford */
+update electorates set population_count = 70297 where id = 3294;
+	
+/* Old Bexley and Sidcup */
+update electorates set population_count = 73198 where id = 3688;
--- lib/tasks/test/8-alter-electorates/alter.sql
+++
@@ -1,5 +0,0 @@
-/* Bexleyheath and Crayford */
-update electorates set population_count = 2 where id = 70297;
-	
-/* Old Bexley and Sidcup */
-update electorates set population_count = 4 where id = 3688;
