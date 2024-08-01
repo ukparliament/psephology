f84c6d0f74a0a433467ba9992378d932b119fb5e
fantasticlife <michael@smethur.st>
2024-08-01 13:38:19 +0100

    DC id updates for old bexley

---
+++ lib/tasks/live/10-correct-dc-ids/correct-dc-ids.sql
@@ -0,0 +1,5 @@
+/* Laurent Williams */
+update candidacies set democracy_club_person_identifier = 117614 where id = 21318;
+	
+/* Andrew Still */
+update candidacies set democracy_club_person_identifier = 117613 where id = 21319;
