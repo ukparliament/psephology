2d208256a7df62e71976824868365cec4d264908
fantasticlife <michael@smethur.st>
2024-08-06 13:49:17 +0100

    basic controller and views

---
+++ app/controllers/general_election_english_region_constituency_area_controller.rb
@@ -0,0 +1,43 @@
+class GeneralElectionEnglishRegionConstituencyAreaController < ApplicationController
+  
+  def index
+    general_election = params[:general_election]
+    @general_election = GeneralElection.find_by_sql(
+      "
+        SELECT ge.*, pp.number AS parliament_period_number
+        FROM general_elections ge, parliament_periods pp
+        WHERE ge.parliament_period_id = pp.id
+        AND ge.id = #{general_election}
+      "
+    ).first
+    
+    country = params[:country]
+    @country = Country.find( country )
+    
+    english_region = params[:english_region]
+    @english_region = EnglishRegion.all.where( 'id = ?', english_region ).where( 'country_id =?', country).first
+    
+    raise ActiveRecord::RecordNotFound unless @general_election and @english_region
+
+    @elections = @general_election.elections_in_english_region( @english_region)
+    
+    @crumb << { label: 'General elections', url: general_election_list_url }
+    @crumb << { label: @general_election.crumb_label, url: general_election_party_list_url }
+    @crumb << { label: @country.name, url: general_election_country_political_party_list_url }
+    @crumb << { label: @english_region.name, url: general_election_english_region_political_party_list_url }
+    @crumb << { label: 'Constituencies', url: nil }
+    @section = 'general-elections'
+    @subsection = 'constituency-areas'
+    
+    if @general_election.is_notional
+      @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by constituency"
+      @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by constituency</span>".html_safe
+      @description = "Notional results for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}, listed by constituencies in #{@english_region.name}, England."
+      render :template => 'general_election_english_region_constituency_area/index_notional'
+    else
+      @page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by constituency"
+      @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by constituency</span>".html_safe
+      @description = "Results for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}, listed by constituencies in #{@english_region.name}, England."
+    end
+  end
+end
---
+++ test/controllers/general_election_english_region_constituency_area_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GeneralElectionEnglishRegionConstituencyAreaControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
