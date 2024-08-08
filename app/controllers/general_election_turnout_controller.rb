4573ec7b3fccb53dead43b1dda20f5e822f4ec38
fantasticlife <michael@smethur.st>
2024-08-08 09:34:46 +0100

    crumb link to general election now links to root, not political parties

--- app/controllers/general_election_turnout_controller.rb
+++ app/controllers/general_election_turnout_controller.rb
@@ -16,7 +16,7 @@ class GeneralElectionTurnoutController < ApplicationController
     
     @csv_url = general_election_turnout_list_url( :format => 'csv' )
     @crumb << { label: 'General elections', url: general_election_list_url }
-    @crumb << { label: @general_election.crumb_label, url: general_election_party_list_url }
+    @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
     @crumb << { label: 'Turnouts', url: nil }
     @section = 'general-elections'
     @subsection = 'turnouts'
