b2376caff2d611ad74a264e854c8c04f570fd1a4
fantasticlife <michael@smethur.st>
2024-08-08 09:35:01 +0100

    crumb link to general election now links to root, not political parties

--- app/controllers/general_election_uncertified_candidacy_controller.rb
+++ app/controllers/general_election_uncertified_candidacy_controller.rb
@@ -19,7 +19,7 @@ class GeneralElectionUncertifiedCandidacyController < ApplicationController
     @description = "Results for a general election to the Parliament of the United Kingdom on #{@general_election.crumb_label} listing candidates not certified by a political party."
     @csv_url = general_election_uncertified_candidacy_list_url( :format => 'csv' )
     @crumb << { label: 'General elections', url: general_election_list_url }
-    @crumb << { label: @general_election.crumb_label, url: general_election_party_list_url }
+    @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
     @crumb << { label: 'Non-party candidates', url: nil }
     @section = 'general-elections'
     @subsection = 'uncertified-candidacies'
