864b58815f4be319d9ddb23be72bd2ccae7d6bd5
fantasticlife <michael@smethur.st>
2024-08-27 22:05:43 +0100

    added respond to for csv

--- app/controllers/general_election_english_region_majority_controller.rb
+++ app/controllers/general_election_english_region_majority_controller.rb
@@ -19,23 +19,31 @@ class GeneralElectionEnglishRegionMajorityController < ApplicationController
   
     @elections = @general_election.elections_by_majority_in_english_region( @english_region )
     
-    @crumb << { label: 'General elections', url: general_election_list_url }
-    @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
-    @crumb << { label: 'England', url: general_election_country_show_url }
-    @crumb << { label: @english_region.name, url: general_election_english_region_show_url }
-    @crumb << { label: 'Majorities', url: nil }
-    @section = 'general-elections'
-    @subsection = 'majorities'
-    
     if @general_election.is_notional
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by majority"
       @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by majority</span>".html_safe
       @description = "Notional results in #{@english_region.name}, England for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}, listed by the majority of the winning candidate."
-      render :template => 'general_election_english_region_majority/index_notional'
     else
       @page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by majority"
       @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by majority</span>".html_safe
       @description = "Results in #{@english_region.name}, England for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}, listed by the majority of the winning candidate."
+    end
+    
+    respond_to do |format|
+      format.csv {
+        response.headers['Content-Disposition'] = "attachment; filename=\"winning-candidate-majorities-in-england-#{@english_region.name.downcase.gsub( ' ', '-' )}-#{'notional-' if @general_election.is_notional}general-election-#{@general_election.polling_on.strftime( '%d-%m-%Y' )}.csv\""
+        render :template => 'general_election_majority/index'
+      }
+      format.html {
+        @crumb << { label: 'General elections', url: general_election_list_url }
+        @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
+        @crumb << { label: 'England', url: general_election_country_show_url }
+        @crumb << { label: @english_region.name, url: general_election_english_region_show_url }
+        @crumb << { label: 'Majorities', url: nil }
+        @section = 'general-elections'
+        @subsection = 'majorities'
+        render :template => 'general_election_english_region_majority/index_notional' if @general_election.is_notional
+      }
     end
   end
 end
