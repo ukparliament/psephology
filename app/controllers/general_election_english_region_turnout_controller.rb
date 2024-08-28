e3bd3fd7160f29565d9868a1ca8a9644be46ee36
fantasticlife <michael@smethur.st>
2024-08-28 13:05:23 +0100

    added respond to for csv

--- app/controllers/general_election_english_region_turnout_controller.rb
+++ app/controllers/general_election_english_region_turnout_controller.rb
@@ -19,23 +19,31 @@ class GeneralElectionEnglishRegionTurnoutController < ApplicationController
   
     @elections = @general_election.elections_by_turnout_in_english_region( @english_region )
     
-    @crumb << { label: 'General elections', url: general_election_list_url }
-    @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
-    @crumb << { label: 'England', url: general_election_country_show_url }
-    @crumb << { label: @english_region.name, url: general_election_english_region_show_url }
-    @crumb << { label: 'Turnouts', url: nil }
-    @section = 'general-elections'
-    @subsection = 'turnouts'
-    
     if @general_election.is_notional
       @page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by turnout"
       @multiline_page_title = "Notional results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by turnout</span>".html_safe
       @description = "Notional results in #{@english_region.name}, England for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}, listed by turnout."
-      render :template => 'general_election_english_region_turnout/index_notional'
     else
       @page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by turnout"
       @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by turnout</span>".html_safe
       @description = "Results in #{@english_region.name}, England for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}, listed by turnout."
+    end
+    
+    respond_to do |format|
+      format.csv {
+        response.headers['Content-Disposition'] = "attachment; filename=\"winning-candidate-turnout-in-england-#{@english_region.name.downcase.gsub( ' ', '-' )}-#{'notional-' if @general_election.is_notional}general-election-#{@general_election.polling_on.strftime( '%d-%m-%Y' )}.csv\""
+        render :template => 'general_election_turnout/index'
+      }
+      format.html {
+        @crumb << { label: 'General elections', url: general_election_list_url }
+        @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
+        @crumb << { label: 'England', url: general_election_country_show_url }
+        @crumb << { label: @english_region.name, url: general_election_english_region_show_url }
+        @crumb << { label: 'Turnouts', url: nil }
+        @section = 'general-elections'
+        @subsection = 'turnouts'
+        render :template => 'general_election_english_region_turnout/index_notional' if @general_election.is_notional
+      }
     end
   end
 end
