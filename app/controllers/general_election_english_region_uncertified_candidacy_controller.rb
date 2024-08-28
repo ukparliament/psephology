591346b4d832fe42dbecd20ed44341fb0f9f98ce
fantasticlife <michael@smethur.st>
2024-08-28 14:54:14 +0100

    added respond to for csv

--- app/controllers/general_election_english_region_uncertified_candidacy_controller.rb
+++ app/controllers/general_election_english_region_uncertified_candidacy_controller.rb
@@ -19,16 +19,23 @@ class GeneralElectionEnglishRegionUncertifiedCandidacyController < ApplicationCo
     
     @uncertified_candidacies = @general_election.uncertified_candidacies_in_english_region( @english_region )
     
-    @page_title = "Results for a UK general election on #{@general_election.crumb_label} - #{@english_region.name}, England - non-party candidates"
-    @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - non-party candidates</span>".html_safe
-    @description = "Non-party candidates in #{@english_region.name}, England for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}."
-    
-    @crumb << { label: 'General elections', url: general_election_list_url }
-    @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
-    @crumb << { label: 'England', url: general_election_country_show_url }
-    @crumb << { label: @english_region.name, url: general_election_english_region_show_url }
-    @crumb << { label: 'Non-party candidates', url: nil }
-    @section = 'general-elections'
-    @subsection = 'uncertified-candidacies'
+    respond_to do |format|
+      format.csv {
+        response.headers['Content-Disposition'] = "attachment; filename=\"uncertified-candidate-data-in-england-#{@english_region.name.downcase.gsub( ' ', '-' )}-#{'notional-' if @general_election.is_notional}general-election-#{@general_election.polling_on.strftime( '%d-%m-%Y' )}.csv\""
+        render :template => 'general_election_uncertified_candidacy/index'
+      }
+      format.html {
+        @page_title = "Results for a UK general election on #{@general_election.crumb_label} - #{@english_region.name}, England - non-party candidates"
+        @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - non-party candidates</span>".html_safe
+        @description = "Non-party candidates in #{@english_region.name}, England for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}."
+        @crumb << { label: 'General elections', url: general_election_list_url }
+        @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
+        @crumb << { label: 'England', url: general_election_country_show_url }
+        @crumb << { label: @english_region.name, url: general_election_english_region_show_url }
+        @crumb << { label: 'Non-party candidates', url: nil }
+        @section = 'general-elections'
+        @subsection = 'uncertified-candidacies'
+      }
+    end
   end
 end
