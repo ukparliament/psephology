2385587735313570fa7d63b58f18579b407ccb38
fantasticlife <michael@smethur.st>
2024-08-28 14:42:39 +0100

    added respond to for csv

--- app/controllers/general_election_english_region_declaration_time_controller.rb
+++ app/controllers/general_election_english_region_declaration_time_controller.rb
@@ -19,15 +19,23 @@ class GeneralElectionEnglishRegionDeclarationTimeController < ApplicationControl
   
     @elections = @general_election.elections_by_declaration_time_in_english_region( @english_region )
     
-    @page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by declaration time"
-    @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by declaration time</span>".html_safe
-    @description = "Results in #{@english_region.name}, England for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}, listed by declaration time."
-    @crumb << { label: 'General elections', url: general_election_list_url }
-    @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
-    @crumb << { label: 'England', url: general_election_country_show_url }
-    @crumb << { label: @english_region.name, url: general_election_english_region_show_url }
-    @crumb << { label: 'Declaration times', url: nil }
-    @section = 'general-elections'
-    @subsection = 'declaration-times'
+    respond_to do |format|
+      format.csv {
+        response.headers['Content-Disposition'] = "attachment; filename=\"declaration-time-data-in-england-#{@english_region.name.downcase.gsub( ' ', '-' )}-#{'notional-' if @general_election.is_notional}general-election-#{@general_election.polling_on.strftime( '%d-%m-%Y' )}.csv\""
+        render :template => 'general_election_turnout/index'
+      }
+      format.html {
+        @page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} - #{@english_region.name}, England - by declaration time"
+        @multiline_page_title = "Results for a UK general election on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )} <span class='subhead'>#{@english_region.name}, England - by declaration time</span>".html_safe
+        @description = "Results in #{@english_region.name}, England for a general election to the Parliament of the United Kingdom on #{@general_election.polling_on.strftime( $DATE_DISPLAY_FORMAT )}, listed by declaration time."
+        @crumb << { label: 'General elections', url: general_election_list_url }
+        @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
+        @crumb << { label: 'England', url: general_election_country_show_url }
+        @crumb << { label: @english_region.name, url: general_election_english_region_show_url }
+        @crumb << { label: 'Declaration times', url: nil }
+        @section = 'general-elections'
+        @subsection = 'declaration-times'
+      }
+    end
   end
 end
