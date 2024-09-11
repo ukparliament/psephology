9557fe2c4fff4e5f87d0dc5ac88f17afe7e7213c
fantasticlife <michael@smethur.st>
2024-09-11 09:15:39 +0100

    added respond to for csv. @enabling_legislation now set as @legislation_items so we can use the same csv template

--- app/controllers/legislation_item_enabling_legislation_controller.rb
+++ app/controllers/legislation_item_enabling_legislation_controller.rb
@@ -12,19 +12,27 @@ class LegislationItemEnablingLegislationController < ApplicationController
     ).first
     raise ActiveRecord::RecordNotFound unless @legislation_item
     
-    @enabling_legislation = @legislation_item.enabling_legislation
-    raise ActiveRecord::RecordNotFound if @enabling_legislation.empty?
+    @legislation_items = @legislation_item.enabling_legislation
+    raise ActiveRecord::RecordNotFound if @legislation_items.empty?
     
-    @page_title = "Enabling legislation for the #{@legislation_item.title}"
-    @multiline_page_title = "Legislation <span class='subhead'>Enabling legislation for the #{@legislation_item.title}</span>".html_safe
-    @description = "Enabling legislation for the #{@legislation_item.title}."
-    if @legislation_item.legislation_type_abbreviation == 'acts'
-      @crumb << { label: 'Acts of Parliament', url: act_of_parliament_list_url }
-    else
-      @crumb << { label: 'Orders in Council', url: order_in_council_list_url }
+    respond_to do |format|
+      format.csv {
+        response.headers['Content-Disposition'] = "attachment; filename=\"enabling-legislation-for-#{@legislation_item.title.downcase.gsub( ' ', '-' )}.csv\""
+        render :template => 'legislation_item/index'
+      }
+      format.html {
+        @page_title = "Enabling legislation for the #{@legislation_item.title}"
+        @multiline_page_title = "Legislation <span class='subhead'>Enabling legislation for the #{@legislation_item.title}</span>".html_safe
+        @description = "Enabling legislation for the #{@legislation_item.title}."
+        if @legislation_item.legislation_type_abbreviation == 'acts'
+          @crumb << { label: 'Acts of Parliament', url: act_of_parliament_list_url }
+        else
+          @crumb << { label: 'Orders in Council', url: order_in_council_list_url }
+        end
+        @crumb << { label: @legislation_item.title, url: legislation_item_show_url }
+        @crumb << { label: 'Enabling legislation', url: nil }
+        @section = 'legislation'
+      }
     end
-    @crumb << { label: @legislation_item.title, url: legislation_item_show_url }
-    @crumb << { label: 'Enabling legislation', url: nil }
-    @section = 'legislation'
   end
 end
