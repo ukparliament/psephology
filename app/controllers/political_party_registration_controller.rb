0aaa88a350e17e424f3f9c5c03051eaa678c6eb0
fantasticlife <michael@smethur.st>
2024-09-19 12:35:22 +0100

    basic controller and views

---
+++ app/controllers/political_party_registration_controller.rb
@@ -0,0 +1,20 @@
+class PoliticalPartyRegistrationController < ApplicationController
+  
+  def index
+    @political_party_registrations = PoliticalPartyRegistration.find_by_sql(
+      "
+        SELECT ppr.*, pp.name AS party_name, pp.legacy_electoral_commission_id AS legacy_electoral_commission_id, c.name AS country_name
+        FROM political_party_registrations ppr, political_parties pp, countries c
+        WHERE ppr.political_party_id = pp.id
+        AND ppr.country_id = c.id
+        ORDER BY pp.name, c.name
+      "
+    )
+    
+    @page_title = 'Political party registrations'
+    @description = "Political parties registered by the Electoral Commissions."
+    @crumb << { label: 'Political parties', url: political_party_winning_list_url }
+    @crumb << { label: 'Registrations', url: nil }
+    @section = 'political-parties'
+  end
+end
---
+++ test/controllers/political_party_registration_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PoliticalPartyRegistrationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
