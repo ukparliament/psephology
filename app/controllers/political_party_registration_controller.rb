42cd48c799aa828f045b315f921afcbdd02e5c06
fantasticlife <michael@smethur.st>
2024-09-23 09:12:58 +0100

    added facility for table sorting

--- app/controllers/political_party_registration_controller.rb
+++ app/controllers/political_party_registration_controller.rb
@@ -11,6 +11,43 @@ class PoliticalPartyRegistrationController < ApplicationController
       "
     )
     
+    # Allow for table sorting.
+    @sort = params[:sort]
+    @order = params[:order]
+    if @order and @sort
+      case @order
+      when 'descending'
+        case @sort
+        when 'party'
+          @political_party_registrations.sort_by! {|registration | registration.party_name}.reverse!
+        when 'registered-on'
+          @political_party_registrations.sort_by! {|registration | registration.start_on}.reverse!
+        when 'deregistered-on'
+          @political_party_registrations.sort_by! {|registration | registration.end_on || Date.new}.reverse!
+        when 'primary-name-last-updated-on'
+          @political_party_registrations.sort_by! {|registration | registration.political_party_name_last_updated_on || Date.new}.reverse!
+        when 'country'
+          @political_party_registrations.sort_by! {|registration | registration.country_name}.reverse!
+        end
+      when 'ascending'
+        case @sort
+        when 'party'
+          @political_party_registrations.sort_by! {|registration | registration.party_name}
+        when 'registered-on'
+          @political_party_registrations.sort_by! {|registration | registration.start_on}
+        when 'deregistered-on'
+          @political_party_registrations.sort_by! {|registration | registration.end_on || Date.new}
+        when 'primary-name-last-updated-on'
+          @political_party_registrations.sort_by! {|registration | registration.political_party_name_last_updated_on || Date.new}
+        when 'country'
+          @political_party_registrations.sort_by! {|registration | registration.country_name}
+        end
+      end
+    else
+      @sort = 'party'
+      @order = 'ascending'
+    end
+    
     @page_title = 'Political party registrations'
     @description = "Political parties registered by the Electoral Commissions."
     @crumb << { label: 'Political parties', url: political_party_winning_list_url }
