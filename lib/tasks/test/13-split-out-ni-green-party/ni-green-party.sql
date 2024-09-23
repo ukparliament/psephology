949486c5b128390ed1424a1101de4c45795e2c59
fantasticlife <michael@smethur.st>
2024-09-23 09:13:29 +0100

    was Northern Ireland Green Party. now Green Party Northern Ireland

--- lib/tasks/test/13-split-out-ni-green-party/ni-green-party.sql
+++ lib/tasks/test/13-split-out-ni-green-party/ni-green-party.sql
@@ -2,4 +2,4 @@ DELETE
 FROM political_party_registrations
 WHERE electoral_commission_id = 'PP305';
 
-UPDATE political_parties SET name = 'Northern Ireland Green Party', mnis_id=1113 where id =267;
+UPDATE political_parties SET name = 'Green Party Northern Ireland', mnis_id=1113 where id =267;
