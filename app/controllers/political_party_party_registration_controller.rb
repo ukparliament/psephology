8798d7601c82cf4b53c79a7de28ad8fdfa42eeaf
fantasticlife <michael@smethur.st>
2024-09-24 10:33:34 +0100

    @registrations now @political_party_registrations so we can use the same csv template as full listing. Added respond to for csv

--- app/controllers/political_party_party_registration_controller.rb
+++ app/controllers/political_party_party_registration_controller.rb
@@ -4,15 +4,24 @@ class PoliticalPartyPartyRegistrationController < ApplicationController
     political_party = params[:political_party]
     @political_party = PoliticalParty.find( political_party )
     
-    @registrations = @political_party.registrations
+    @political_party_registrations = @political_party.registrations
     
-    @page_title = "#{@political_party.name} - registrations"
-    @multiline_page_title = "#{@political_party.name} <span class='subhead'>Registrations</span>".html_safe
-    @description = "Registrations of #{@political_party.name} by the Electoral Commissions."
-    @crumb << { label: 'Political parties', url: political_party_winning_list_url }
-    @crumb << { label: @political_party.name, url: political_party_show_url }
-    @crumb << { label: 'Registrations', url: nil }
-    @section = 'political-parties'
-    @subsection = 'registration'
+    respond_to do |format|
+      format.csv {
+        response.headers['Content-Disposition'] = "attachment; filename=\"political-party-registrations.csv\""
+        render :template => 'political_party_registration/index'
+      }
+      format.html {
+        @page_title = "#{@political_party.name} - registrations"
+        @multiline_page_title = "#{@political_party.name} <span class='subhead'>Registrations</span>".html_safe
+        @description = "Registrations of #{@political_party.name} by the Electoral Commissions."
+        @csv_url = political_party_party_registration_list_url( :format => 'csv' )
+        @crumb << { label: 'Political parties', url: political_party_winning_list_url }
+        @crumb << { label: @political_party.name, url: political_party_show_url }
+        @crumb << { label: 'Registrations', url: nil }
+        @section = 'political-parties'
+        @subsection = 'registration'
+      }
+    end
   end
 end
