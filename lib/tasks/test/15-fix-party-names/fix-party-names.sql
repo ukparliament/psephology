8b9245853fcdf3af82c6a4c0aef7c10823c30358
fantasticlife <michael@smethur.st>
2024-09-25 20:22:56 +0100

    patches for party names

---
+++ lib/tasks/test/15-fix-party-names/fix-party-names.sql
@@ -0,0 +1,5 @@
+update political_parties set name='Christian Peoples Alliance Party' where id=54;
+update political_parties set name='Common Good Party' where id=131;
+update political_parties set name='The Peace Party' where id=57;
+update political_parties set name='Alliance for Green Socialism' where id=84;
+update political_parties set name='Lincolnshire Independents' where id=73;
