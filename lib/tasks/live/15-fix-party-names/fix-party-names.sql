d608d5dd49a02557f821d92c1e7effffe47fc928
fantasticlife <michael@smethur.st>
2024-09-25 20:26:31 +0100

    moved to live

--- lib/tasks/live/15-fix-party-names/fix-party-names.sql
+++ lib/tasks/live/15-fix-party-names/fix-party-names.sql
@@ -3,3 +3,4 @@ update political_parties set name='Common Good Party' where id=131;
 update political_parties set name='The Peace Party' where id=57;
 update political_parties set name='Alliance for Green Socialism' where id=84;
 update political_parties set name='Lincolnshire Independents' where id=73;
+update political_parties set name='Save Hartlepool Hospital' where id=80;
