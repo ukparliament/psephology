db4e7e5e283e6640f111d2ff48a155ac7df8c5ed
fantasticlife <michael@smethur.st>
2024-09-26 11:55:12 +0100

    added more attribute accessors to cope with by-election constituency stuff in csv. added parliament period heading

--- app/models/election_listing_item.rb
+++ app/models/election_listing_item.rb
@@ -3,5 +3,22 @@ class ElectionListingItem
   attr_accessor :id
   attr_accessor :type
   attr_accessor :polling_on
+  attr_accessor :parliament_period_number
+  attr_accessor :parliament_period_summoned_on
+  attr_accessor :parliament_period_dissolved_on
+  attr_accessor :parliament_period_wikidata_id
+  attr_accessor :parliament_period_london_gazette
+  attr_accessor :parliament_period_id
   attr_accessor :constituency_group_name
+  attr_accessor :constituency_area_geographic_code
+  attr_accessor :constituency_area_type
+  attr_accessor :constituency_area_id
+  
+  def parliament_period_heading
+    parliament_period_heading = "#{self.parliament_period_number.ordinalize} Parliament (#{self.parliament_period_summoned_on.strftime( $DATE_DISPLAY_FORMAT )} -"
+    parliament_period_heading += " #{self.parliament_period_dissolved_on.strftime( $DATE_DISPLAY_FORMAT )}" if self.parliament_period_dissolved_on
+    parliament_period_heading += ")"
+    parliament_period_heading
+  end
 end
+
