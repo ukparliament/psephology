a818c2c437518d8a0b43654e65d28e4c07d6ccf1
fantasticlife <michael@smethur.st>
2024-10-02 13:34:13 +0100

    now also return by-elections

--- app/controllers/boundary_set_general_election_majority_controller.rb
+++ app/controllers/boundary_set_general_election_majority_controller.rb
@@ -1,4 +1,6 @@
 class BoundarySetGeneralElectionMajorityController < ApplicationController
+
+  # This controller is now badly named, also including by-elections.
   
   def index
     boundary_set = params[:boundary_set]
@@ -13,12 +15,12 @@ class BoundarySetGeneralElectionMajorityController < ApplicationController
     ).first
     raise ActiveRecord::RecordNotFound unless @boundary_set
     
-    # We get all elections held in a constituency area defined by the boundary set.
+    # We get all elections held in a constituency area defined by the boundary set, to include by-elections.
     @elections = @boundary_set.all_elections
     
-    @page_title = "Boundary set for #{@boundary_set.display_title} - general election constituency majorities"
-    @multiline_page_title = "Boundary set for #{@boundary_set.display_title} <span class='subhead'>General elections constituency majorities</span>".html_safe
-    @description = "Majorities of winning candidates in general elections during the existence of the #{@boundary_set.display_title} boundary set."
+    @page_title = "Boundary set for #{@boundary_set.display_title} - majorities"
+    @multiline_page_title = "Boundary set for #{@boundary_set.display_title} <span class='subhead'>Majorities</span>".html_safe
+    @description = "Majorities of winning candidates in elections during the existence of the #{@boundary_set.display_title} boundary set."
     @crumb << { label: 'Boundary sets', url: boundary_set_list_url }
     @crumb << { label: @boundary_set.display_title, url: boundary_set_show_url( :boundary_set => @boundary_set ) }
     @crumb << { label: 'Majorities', url: nil }
