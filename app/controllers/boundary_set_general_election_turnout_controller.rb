5474fcac7ef33e64b5e3337ebd41231c9cdba902
fantasticlife <michael@smethur.st>
2024-10-02 13:35:20 +0100

    now also return by-elections

--- app/controllers/boundary_set_general_election_turnout_controller.rb
+++ app/controllers/boundary_set_general_election_turnout_controller.rb
@@ -1,4 +1,6 @@
 class BoundarySetGeneralElectionTurnoutController < ApplicationController
+
+  # This controller is now badly named, also including by-elections.
   
   def index
     boundary_set = params[:boundary_set]
@@ -13,12 +15,12 @@ class BoundarySetGeneralElectionTurnoutController < ApplicationController
     ).first
     raise ActiveRecord::RecordNotFound unless @boundary_set
     
-    # We get all elections held in a constituency area defined by the boundary set.
-    @elections = @boundary_set.elections_in_general_elections
+    # We get all elections held in a constituency area defined by the boundary set, to include by-elections.
+    @elections = @boundary_set.all_elections
     
-    @page_title = "Boundary set for #{@boundary_set.display_title} - general election constituency turnouts"
-    @multiline_page_title = "Boundary set for #{@boundary_set.display_title} <span class='subhead'>General elections constituency turnouts</span>".html_safe
-    @description = "Turnouts in general elections in #{@boundary_set.country_name} held during the existence of the #{@boundary_set.display_title} boundary set."
+    @page_title = "Boundary set for #{@boundary_set.display_title} - turnouts"
+    @multiline_page_title = "Boundary set for #{@boundary_set.display_title} <span class='subhead'>Turnouts</span>".html_safe
+    @description = "Turnouts in elections in #{@boundary_set.country_name} held during the existence of the #{@boundary_set.display_title} boundary set."
     @crumb << { label: 'Boundary sets', url: boundary_set_list_url }
     @crumb << { label: @boundary_set.display_title, url: boundary_set_show_url( :boundary_set => @boundary_set ) }
     @crumb << { label: 'Turnouts', url: nil }
