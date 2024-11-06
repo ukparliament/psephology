86bbdb7b4eeffcbfd6d20bb545067c5c79ad1c6e
fantasticlife <michael@smethur.st>
2024-11-06 09:12:30 +0000

    added election writ issued text method

--- app/helpers/election_helper.rb
+++ app/helpers/election_helper.rb
@@ -1,2 +1,13 @@
 module ElectionHelper
+
+  def election_writ_issued_text( election )
+  if election.writ_issued_on
+      if election.general_election
+        election_writ_issued_text = content_tag( 'p', "The writ for this election was issued on #{election.writ_issued_on.strftime( $DATE_DISPLAY_FORMAT )}." )
+      else
+        election_writ_issued_text = content_tag( 'p', "The writ for this by-election was issued on #{election.writ_issued_on.strftime( $DATE_DISPLAY_FORMAT )}." )
+      end
+    end
+    election_writ_issued_text if election_writ_issued_text
+  end
 end
