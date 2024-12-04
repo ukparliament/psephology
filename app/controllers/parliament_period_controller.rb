4789ad4ee88de4d7cbd0e9b4b7166c68e32f61f1
fantasticlife <michael@smethur.st>
2024-12-04 21:54:53 +0000

    added method to render parliament periods as a list with elections, not a table

--- app/controllers/parliament_period_controller.rb
+++ app/controllers/parliament_period_controller.rb
@@ -37,6 +37,46 @@ class ParliamentPeriodController < ApplicationController
     @section = 'parliament-periods'
   end
   
+  def index_as_list
+    @parliament_periods = ParliamentPeriod.find_by_sql(
+      "
+        SELECT
+          pp.*,
+          general_election.polling_on AS general_election_polling_on,
+          general_election.id AS general_election_id,
+          by_election.count AS by_election_count
+        FROM parliament_periods pp
+        
+        INNER JOIN (
+          SELECT *
+          FROM general_elections
+          WHERE is_notional IS FALSE
+        ) general_election
+        ON general_election.parliament_period_id = pp.id
+        
+        LEFT JOIN (
+          SELECT count(id) AS count, parliament_period_id
+          FROM elections
+          WHERE general_election_id IS NULL
+          AND  is_notional IS FALSE
+          GROUP BY parliament_period_id
+        ) by_election
+        ON by_election.parliament_period_id = pp.id
+        
+        ORDER BY summoned_on desc
+      "
+    )
+    
+    @page_title = 'Elections by Parliament period'
+    @multiline_page_title = "Elections <span class='subhead'>By Parliament period</span>".html_safe
+    @description = 'Elections by Parliament period.'
+    @csv_url = parliament_period_list_url( :format => 'csv' )
+    @crumb << { label: 'Elections by Parliament period', url: nil }
+    @section = 'parliament-periods'
+  end
+  
+  
+  
   def show
     parliament_period = params[:parliament_period]
     @parliament_period = ParliamentPeriod.find_by_number( parliament_period )
