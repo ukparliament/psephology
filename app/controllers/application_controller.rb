30894ac274ab3757bfa555710b54340c3a00f517
fantasticlife <michael@smethur.st>
2025-01-20 22:44:04 +0000

    added new constant for crumb date display format to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base 
   
   $DATE_DISPLAY_FORMAT = '%-d %B %Y'
+  $CRUMB_DATE_DISPLAY_FORMAT = '%B %Y'
   $DATE_TIME_DISPLAY_FORMAT = '%H:%M on %-d %B %Y'
   $DECLARATION_TIME_DISPLAY_FORMAT = '%A %-d at %H:%M'
   $TIME_DISPLAY_FORMAT = '%H:%M'
