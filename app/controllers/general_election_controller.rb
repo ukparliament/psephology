cd4c34ace7b754a1d4e44c88a73effd483c5d78f
fantasticlife <michael@smethur.st>
2025-01-21 10:25:31 +0000

    general election show method no longer directly queries for the general election which is grabbed in the application controller instead

--- app/controllers/general_election_controller.rb
+++ app/controllers/general_election_controller.rb
@@ -39,17 +39,6 @@ class GeneralElectionController < ApplicationController
   end
   
   def show
-    general_election = params[:general_election]
-    @general_election = GeneralElection.find_by_sql(
-      "
-        SELECT ge.*, pp.number AS parliament_period_number
-        FROM general_elections ge, parliament_periods pp
-        WHERE ge.parliament_period_id = pp.id
-        AND ge.id = #{general_election}
-      "
-    ).first
-    raise ActiveRecord::RecordNotFound unless @general_election
-    
     @party_performances = @general_election.party_performance
     
     respond_to do |format|
