8bafd816660e6bdea72ec9d5e58c223696bf3918
fantasticlife <michael@smethur.st>
2025-01-27 17:26:42 +0000

    added call to political_party.political_parties_sharing_registrations in political party general election list

--- app/controllers/political_party_general_election_controller.rb
+++ app/controllers/political_party_general_election_controller.rb
@@ -3,8 +3,8 @@ class PoliticalPartyGeneralElectionController < ApplicationController
   def index
     political_party = params[:political_party]
     @political_party = PoliticalParty.find( political_party )
-    
     @general_elections = @political_party.general_elections
+    @political_parties_sharing_registrations = @political_party.political_parties_sharing_registrations
     
     @page_title = "#{@political_party.name} - general elections"
     @multiline_page_title = "#{@political_party.name} <span class='subhead'>General elections</span>".html_safe
