d9d0981aa1bc1f19cf96ab43c6a5b6b3ee923511
James Jefferies <james@shedcode.co.uk>
2025-02-28 15:08:57 +0000

    Add smoke test

---
+++ spec/system/system/smoke_test_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "smoke test", type: :system do
+
+  # We don't have any javascript, so can just use rack test
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'home page loads' do
+    visit root_path
+    expect(page).to have_content("Committees")
+  end
+end
