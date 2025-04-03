4d98c2a194e7181caa7ee716ffc6b3482e574f21
fantasticlife <michael@smethur.st>
2025-04-03 12:06:02 +0100

    first pass constituency name changes on test

---
+++ lib/tasks/test/24-fix-party-names/fix-party-names.sql
@@ -0,0 +1,7 @@
+UPDATE political_parties SET name='Christian Peoples Alliance' WHERE id=54; -- was Christian Peoples Alliance Party
+
+UPDATE political_parties SET name='Common Good' WHERE id=131; -- was Common Good Party
+
+UPDATE political_parties SET name='Lincolnshire Independents Lincolnshire First' WHERE id=73; -- was Lincolnshire Independents
+
+UPDATE political_parties SET name='The Peace Party - Non-violence, Justice, Environment' WHERE id=57; -- was The Peace Party
