4220e47d4d70c08c43c131e397f686b25da83b3b
fantasticlife <michael@smethur.st>
2025-04-22 09:22:25 +0100

    sql to add two new dashboards

---
+++ lib/tasks/26-add-new-dashboards/add-new-dashboards.sql
@@ -0,0 +1,17 @@
+INSERT INTO commons_library_dashboards (title, url) VALUES ('Sewage discharges in England', 'https://commonslibrary.parliament.uk/constituency-data-sewage-discharges-in-england/');
+
+INSERT INTO commons_library_dashboards (title, url) VALUES ('Census data: How do constituencies with different demographics vote?', 'https://commonslibrary.parliament.uk/census-data-how-do-constituencies-with-different-demographics-vote/');
+
+
+INSERT INTO commons_library_dashboard_countries (commons_library_dashboard_id, country_id) VALUES (5, 2);
+
+INSERT INTO commons_library_dashboard_countries (commons_library_dashboard_id, country_id) VALUES (6, 2);
+INSERT INTO commons_library_dashboard_countries (commons_library_dashboard_id, country_id) VALUES (6, 4);
+INSERT INTO commons_library_dashboard_countries (commons_library_dashboard_id, country_id) VALUES (6, 5);
+INSERT INTO commons_library_dashboard_countries (commons_library_dashboard_id, country_id) VALUES (6, 6);
+
+
+
+
+
+
