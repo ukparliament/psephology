d37c752a13da6d2345ebf4e901a7bd029e6e4a4a
fantasticlife <michael@smethur.st>
2025-04-28 16:14:55 +0100

    parliament period maiden speeches method no longer queries across party table

--- app/models/parliament_period.rb
+++ app/models/parliament_period.rb
@@ -54,10 +54,6 @@ class ParliamentPeriod < ApplicationRecord
           member.mnis_id AS member_mnis_id,
           constituency_group.name AS constituency_group_name,
           constituency_area_id AS constituency_area_id,
-          main_political_party.id AS main_political_party_id,
-          main_political_party.name AS main_political_party_name,
-          adjunct_political_party.id AS adjunct_political_party_id,
-          adjunct_political_party.name AS adjunct_political_party_name,
           parliament_period.number AS parliament_period_number
         
         FROM maiden_speeches ms
@@ -67,18 +63,6 @@ class ParliamentPeriod < ApplicationRecord
           FROM members
         ) member
         ON member.id = ms.member_id
-      
-        LEFT JOIN (
-          SELECT *
-          FROM political_parties
-        ) main_political_party
-        ON main_political_party.id = ms.main_political_party_id
-      
-        LEFT JOIN (
-          SELECT *
-          FROM political_parties
-        ) adjunct_political_party
-        ON adjunct_political_party.id = ms.adjunct_political_party_id
       
         INNER join (
           SELECT *
