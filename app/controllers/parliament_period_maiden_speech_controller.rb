2a822b3b867c19c14f595771fe8c0d01f5bff972
fantasticlife <michael@smethur.st>
2025-05-07 20:52:59 +0100

    allow for table sorting on parliament period maiden speech list action

--- app/controllers/parliament_period_maiden_speech_controller.rb
+++ app/controllers/parliament_period_maiden_speech_controller.rb
@@ -7,6 +7,39 @@ class ParliamentPeriodMaidenSpeechController < ApplicationController
     
     @maiden_speeches = @parliament_period.maiden_speeches
     
+    # Allow for table sorting.
+    @sort = params[:sort]
+    @order = params[:order]
+    if @order and @sort
+      case @order
+      when 'descending'
+        case @sort
+        when 'member-name'
+          @maiden_speeches.sort_by! {|ms| ms.member_family_name}.reverse! # add given name here
+        when 'constituency-name'
+          @maiden_speeches.sort_by! {|ms| ms.constituency_group_name}.reverse!
+        when 'date-made'
+          @maiden_speeches.sort_by! {|ms| ms.made_on}.reverse!
+        when 'session'
+          @maiden_speeches.sort_by! {|ms| ms.session_number}.reverse!
+        end
+      when 'ascending'
+        case @sort
+        when 'member-name'
+          @maiden_speeches.sort_by! {|ms| ms.member_family_name}
+        when 'constituency-name'
+          @maiden_speeches.sort_by! {|ms| ms.constituency_group_name}
+        when 'date-made'
+          @maiden_speeches.sort_by! {|ms| ms.made_on}
+        when 'session'
+          @maiden_speeches.sort_by! {|ms| ms.session_number}
+        end
+      end
+    else
+      @sort = 'date-made'
+      @order = 'ascending'
+    end
+    
     @page_title = "#{@parliament_period.number.ordinalize} Parliament of the United Kingdom - maiden speeches"
     @multiline_page_title = "#{@parliament_period.number.ordinalize} Parliament of the United Kingdom <span class='subhead'>Maiden speeches</span>".html_safe
     @description = "Maiden speeches made during the #{@parliament_period.number.ordinalize} Parliament of the United Kingdom."
