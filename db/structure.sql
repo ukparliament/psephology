02a01ac4d47cca024927fd0f8a67e89c294e81ad
James Jefferies <james@shedcode.co.uk>
2025-07-19 16:05:56 +0100

    Update SQL based on production database

--- db/structure.sql
+++ db/structure.sql
@@ -1,6 +1,7 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -8,20 +9,6 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- Name: heroku_ext; Type: SCHEMA; Schema: -; Owner: -
---
-
-CREATE SCHEMA heroku_ext;
-
-
---
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON SCHEMA public IS '';
-
 
 --
 -- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
@@ -941,9 +928,7 @@ CREATE TABLE public.maiden_speeches (
     hansard_url character varying(255) NOT NULL,
     member_id integer NOT NULL,
     constituency_group_id integer NOT NULL,
-    parliament_period_id integer NOT NULL,
-    main_political_party_id integer,
-    adjunct_political_party_id integer
+    parliament_period_id integer NOT NULL
 );
 
 
@@ -1829,14 +1814,6 @@ CREATE INDEX index_legislation_items_on_legislation_type_id ON public.legislatio
 
 
 --
--- Name: maiden_speeches fk_adjunct_political_party; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.maiden_speeches
-    ADD CONSTRAINT fk_adjunct_political_party FOREIGN KEY (adjunct_political_party_id) REFERENCES public.political_parties(id);
-
-
---
 -- Name: certifications fk_adjunct_to_certification; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -2130,14 +2107,6 @@ ALTER TABLE ONLY public.constituency_group_set_legislation_items
 
 ALTER TABLE ONLY public.legislation_items
     ADD CONSTRAINT fk_legislation_type FOREIGN KEY (legislation_type_id) REFERENCES public.legislation_types(id);
-
-
---
--- Name: maiden_speeches fk_main_political_party; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.maiden_speeches
-    ADD CONSTRAINT fk_main_political_party FOREIGN KEY (main_political_party_id) REFERENCES public.political_parties(id);
 
 
 --
