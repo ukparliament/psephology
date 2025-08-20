8ccdedb6a74397a1b8107d8eafa17c34d5fc35ee
fantasticlife <michael@smethur.st>
2025-08-20 11:19:25 +0100

    tidied result summary tidy sql

--- lib/tasks/test/29-tidy-unused-result-summaries/tidy-unused-result-summaries.sql
+++ lib/tasks/test/29-tidy-unused-result-summaries/tidy-unused-result-summaries.sql
@@ -1,6 +1,14 @@
 DELETE FROM result_summaries WHERE id=58;
 DELETE FROM result_summaries WHERE id=59;
-DELETE FROM result_summaries WHERE id=61;
-DELETE FROM result_summaries WHERE id=62;
-DELETE FROM result_summaries WHERE id=63;
-DELETE FROM result_summaries WHERE id=64;
+
+/*
+SELECT rs.id, election.count
+FROM result_summaries rs
+LEFT JOIN (
+	SELECT result_summary_id, count(id) AS count
+	FROM elections
+	GROUP BY result_summary_id
+) AS election
+ON election.result_summary_id = rs.id
+WHERE election.count IS NULL;
+*/
