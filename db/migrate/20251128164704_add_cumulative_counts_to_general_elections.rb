d0de08d5737bd9bbf170456eccaf2baac465fa68
fantasticlife <michael@smethur.st>
2025-12-02 17:33:23 +0000

    remove migrations to remove and re-add general election cumulative counts

--- db/migrate/20251128164704_add_cumulative_counts_to_general_elections.rb
+++
@@ -1,7 +0,0 @@
-class AddCumulativeCountsToGeneralElections < ActiveRecord::Migration[8.1]
-  def change
-    add_column :general_elections, :valid_vote_count, :int
-    add_column :general_elections, :invalid_vote_count, :int
-    add_column :general_elections, :electorate_population_count, :int
-  end
-end
