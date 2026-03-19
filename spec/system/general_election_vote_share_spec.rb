cd3b44649ab3c92891626613c9a9b3747ff1686b
twaitesr <twaitesr@parliament.uk>
2026-03-19 16:01:17 +0000

    check the correct page actually loads based on test presented

--- spec/system/general_election_vote_share_spec.rb
+++ spec/system/general_election_vote_share_spec.rb
@@ -1,3 +1,21 @@
 require 'rails_helper'
 
-describe "Test for checking page load and data accuracy of general_election/:general_election/vote-share" type: :system do
+describe "Test for checking page load and data accuracy of general_election/:general_election/vote-share 20244 07 04 GE" type: :system do
+	let(:polling_date)	{'2024-07-04'}
+	let(:polling_date_as_display)     { '4 July 2024' }
+
+
+	it 'loads general-election/:general-election/vote-share based on 2024 07 04 GE ' do
+		#get the general election for the selected polling on date (most recent at time of writing)
+			generalelection = GeneralElection.where(polling_on: polling_date).last.id
+
+			#pass to general_election_list_path
+			visit general_election_vote_share_list_path(generalelection)
+
+			#check content on page suggests correct page?
+			expect(page).to have_content("Results for the UK general election on #{polling_date_as_display}")
+			expect(page).to have_content("By vote share")
+		
+	end
+
+	
