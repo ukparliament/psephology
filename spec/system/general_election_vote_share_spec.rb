592149798c590a84eaa450307988f01f1c06fad0
twaitesr <twaitesr@parliament.uk>
2026-03-19 16:15:48 +0000

    change to orginal test due to typos causing failure and new second test to check results

--- spec/system/general_election_vote_share_spec.rb
+++ spec/system/general_election_vote_share_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
-describe "Test for checking page load and data accuracy of general_election/:general_election/vote-share 20244 07 04 GE" type: :system do
+describe "Test for checking page load and data accuracy of general_election/:general_election/vote-share 2024 07 04 GE", type: :system do
+	#have to hard code date choosen
 	let(:polling_date)	{'2024-07-04'}
 	let(:polling_date_as_display)     { '4 July 2024' }
+	#difficult to check the majority via simple db query so hard coding no changes to be expected to data at this point
+	let(:first_line_of_results_table) {[ '1', 'Chorley', 'HOYLE, Lindsay', 'Spk', '74.3%' ]}
 
 
 	it 'loads general-election/:general-election/vote-share based on 2024 07 04 GE ' do
@@ -18,4 +21,21 @@ describe "Test for checking page load and data accuracy of general_election/:gen
 		
 	end
 
-	
+	it 'has the same first line of results for the 2024 07 04 election that was there last time' do
+		#get the general election for the selected polling on date (most recent at time of writing)
+			generalelection = GeneralElection.where(polling_on: polling_date).last.id
+
+			#pass to general_election_list_path
+			visit general_election_vote_share_list_path(generalelection)
+
+			#check content on page suggests correct page?
+			first_data_row = all('tbody tr').first
+
+       		cells = first_data_row.all('td').map(&:text)
+
+       		expect(cells).to match_array(first_line_of_results_table)
+		
+	end
+
+end
+
