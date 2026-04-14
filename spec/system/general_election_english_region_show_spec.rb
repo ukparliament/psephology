5c46c8cb1a4d37d18cdec8239bc7b5c3978866d2
twaitesr <twaitesr@parliament.uk>
2026-04-14 13:03:40 +0100

    test for first english region for 2024 07 04 GE checking correct page loads and first line of results

---
+++ spec/system/general_election_english_region_show_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+describe "Test for checking page load and data accuracy of general_election/:general_election/countries/2/english-regions/:english-region 2024 07 04 GE", type: :system do
+	#have to hard code date chosen
+	let(:polling_date)	{'2024-07-04'}
+	let(:polling_date_as_display)     { '4 July 2024' } 
+	#difficult to check the first line via simple db query so hard coding no changes to be expected to data at this point for 2024 GE
+	let(:first_line_of_results_table) {[ 'Labour', '91', '1,067,897','24.5%', '36' ]}
+	#			
+						
+
+	it 'loads general-election/:general-election/countries/2/english-regions/:english-region based on 2024 07 04 GE' do
+		#get the general election for the selected polling on date (most recent at time of writing)
+			generalelection = GeneralElection.where(polling_on: polling_date).last.id
+
+		#get the country - first with parent id is England
+			country = Country.where(parent_country_id: '1').first.id
+			countryName = Country.where(parent_country_id: '1').first.name
+
+		#get the first english region - south west
+			englishregion = EnglishRegion.first.id
+			englishregionName = EnglishRegion.first.name
+
+
+		#pass to general_election_list_path
+			visit general_election_english_region_show_path(generalelection, country, englishregion)
+
+		#check content on page suggests correct page?
+			expect(page).to have_content("Results for the UK general election on #{polling_date_as_display}")
+			expect(page).to have_content("#{englishregionName}, #{countryName} - by party")
+		
+	end
+
+	it 'has the same first line of results for the 2024 07 04 election that was there last time' do
+		#get the general election for the selected polling on date (most recent at time of writing)
+			generalelection = GeneralElection.where(polling_on: polling_date).last.id
+
+
+		#get the country - first with parent id is England
+			country = Country.where(parent_country_id: '1').first.id
+			countryName = Country.where(parent_country_id: '1').first.name
+
+		#get the first english region - south west
+			englishregion = EnglishRegion.first.id
+			englishregionName = EnglishRegion.first.name
+
+
+			#pass to general_election_list_path\
+			visit general_election_english_region_show_path(generalelection, country, englishregion)
+
+			#check content on page suggests correct page?
+			first_data_row = all('tbody tr').first
+
+       		cells = first_data_row.all('td').map(&:text)
+
+       		expect(cells).to match_array(first_line_of_results_table)
+		
+	end
+
+end
