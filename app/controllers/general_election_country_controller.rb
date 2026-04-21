201aa85fcab82631041873b4eeb8e6a26d2bc338
fantasticlife <michael@smethur.st>
2026-04-21 11:54:44 +0100

    reworked general election country show controller to work with new state property

--- app/controllers/general_election_country_controller.rb
+++ app/controllers/general_election_country_controller.rb
@@ -29,7 +29,9 @@ class GeneralElectionCountryController < ApplicationController
         @page_title = "#{@general_election.common_title} - #{@country.name} - by party"
         @multiline_page_title = "#{@general_election.common_title} <span class='subhead'>#{@country.name} - by party</span>".html_safe
         @description = "#{@general_election.common_description} in #{@country.name}, listed by political party."
-        @csv_url = general_election_country_political_party_list_url( :format => 'csv' )
+        if @general_election.state == 4
+          @csv_url = general_election_country_political_party_list_url( :format => 'csv' )
+        end
         @crumb << { label: 'Parliament periods', url: parliament_period_list_url }
         @crumb << { label: @general_election.parliament_period_crumb_label, url: parliament_period_show_url( :parliament_period => @general_election.parliament_period_number) }
         @crumb << { label: @general_election.crumb_label, url: general_election_show_url }
@@ -39,11 +41,11 @@ class GeneralElectionCountryController < ApplicationController
         
         if @general_election.is_notional
           render :template => 'general_election_country_political_party/index_notional'
-        elsif @general_election.publication_state == 0
+        elsif @general_election.state == 1
           render :template => 'general_election_country_political_party/index_dissolution'
-        elsif @general_election.publication_state == 1
+        elsif @general_election.state == 2
           render :template => 'general_election_country_political_party/index_candidates_only'
-        elsif @general_election.publication_state == 2
+        elsif @general_election.state == 3
           render :template => 'general_election_country_political_party/index_winners_only'
         else
           render :template => 'general_election_country_political_party/index'
